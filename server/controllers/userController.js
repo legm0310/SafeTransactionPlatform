26e961622713917c39d496d9e7af14c5237898a9
Lee gyu min <legm0310@gmail.com>
2023-04-25 08:02:51 +0900

    Feat: 모델 메서드추가, 로그인 로직 추가

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,19 +1,42 @@
-const User = require("../models/User");
+const db = require("../models");
 module.exports = {
   signup: async (req, res) => {
     let newUser = {
+      role: 1,
       user_name: req.body.user_name,
       email: req.body.email,
       password: req.body.password,
       phone_number: req.body.phone_number,
     };
-    await User.create(newUser).catch((err) => console.log(err));
-    res.status(200).json({
-      result: "complete",
-      state: 200,
-    });
+    try {
+      const findUser = await db.User.getUserByPhoneNumber(newUser.phone_number);
+
+      if (findUser != null) res.status(409).json({ result: "exist user" });
+      await db.User.create(newUser);
+    } catch (err) {
+      throw err;
+    } finally {
+      res.status(200).json({
+        result: "signupSuccess.",
+      });
+    }
   },
-  login: (req, res) => {
-    console.log(res.console.log(123));
+
+  login: async (req, res) => {
+    const user = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+
+    const findUser = await db.User.findOne({ where: { email: user.email } });
+    if (!findUser) res.status(403).json({ result: "user not found" });
+    else if (!(await findUser.validPassword(user.password, findUser.password)))
+      res.status(403).json({
+        result: "Invalid password",
+      });
+    else
+      res.status(200).json({
+        result: "loginSuccess",
+      });
   },
 };
