714ff20d924abdfa8d262ae6ceda5ed4d7664866
legm0310 <legm0310@email.com>
2023-04-24 16:23:39 +0900

    Refactor: 주석 제거

--- server/models/user.js
+++ server/models/user.js
@@ -51,21 +51,5 @@ const User = sequelize.define(
     timestamps: false, // This will add createdAt and updatedAt fields
   }
 );
-// async () => await User.sync({});
-// (async () => {
-//   await sequelize.sync();
-//   console.log("User table created or updated");
-
-//   const newUser = await User.create({
-//     wallet_address: "0x1234567890abcdef",
-//     role: 1,
-//     user_name: "John Doe",
-//     email: "john.doe@example.com",
-//     password: "secret",
-//     phone_number: "123-456-7890",
-//   });
-
-//   console.log("New user created:", newUser.toJSON());
-// })();
 
 module.exports = User;
