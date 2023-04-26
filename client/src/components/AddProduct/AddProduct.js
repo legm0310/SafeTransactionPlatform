bf1552ad0e046fd12ea8be957a98df3dae67d009
junhyeon0218 <82866220+junhyeon0218@users.noreply.github.com>
2023-04-26 23:31:37 +0900

    Refactor: 상품등록 폼형식으로 변환

--- client/src/components/AddProduct/AddProduct.js
+++ client/src/components/AddProduct/AddProduct.js
@@ -6,6 +6,8 @@ const AddProduct = () => {
     const [price, setPrice] = useState("");
     const [nameLength, setNameLength] = useState(0);
 
+    
+
     const handleNameChange = (event) => {
         const value = event.target.value;
         setName(value);
@@ -19,35 +21,47 @@ const AddProduct = () => {
             setPrice(newPrice);
         }
     };
+    const handleSubmit = (event) => {
+        event.preventDefault(); // prevent form submission
+        if (name.trim() === "") {
+            alert("상품 이름을 입력해주세요.");
+            return;
+        }
+        if (price === "") {
+            alert("상품 가격을 입력해주세요.");
+            return;
+        }
+        // code to submit the form
+    }
 
 
 
     return (
         <Fragment>
-            <ul className={classes.form}>
+            <form className={classes.form}>
                 <div className={classes.title}>상품 등록하기</div>
-                <li className={classes.label}>
+                <div className={classes.label}>
                     <div className={classes.labelTitle}>
                         사진 등록
                     </div>
                     <div>
-                        <form className={classes.imgSubmit}>
+                        <div className={classes.imgSubmit}>
                             <label htmlFor="imgUpload" className={classes.imgUploadTitle}>이미지 등록 +</label>
                             <input id={classes.imgUpload} type="file" accept="image/*" />
                             
-                        </form>
+                        </div>
                         <ul className={classes.imgExplain}>
                             * 상품 이미지는 640x640에 최적화 되어 있습니다.
-                            <li>- 상품 이미지는 PC에서는 1:1, 모바일에서는 1:1.23 비율로 보여집니다.</li>
-                            <li>- 이미지는 상품 등록 시 정사각형으로 잘려서 등록됩니다.</li>
-                            <li>- 이미지를 클릭할 경우 원본 이미지를 확인할 수 있습니다.</li>
-                            <li>- 이미지를 클릭 후 이동하여 등록순서를 변경할 수 있습니다.</li>
-                            <li>- 큰 이미지일 경우 이미지가 깨지는 경우가 발생할 수 있습니다.</li>
+                            <li> - 상품 이미지는 PC에서는 1:1, 모바일에서는 1:1.23 비율로 보여집니다.</li>
+                            <li> - 이미지는 상품 등록 시 정사각형으로 잘려서 등록됩니다.</li>
+                            <li> - 이미지를 클릭할 경우 원본 이미지를 확인할 수 있습니다.</li>
+                            <li> - 이미지를 클릭 후 이동하여 등록순서를 변경할 수 있습니다.</li>
+                            <li> - 큰 이미지일 경우 이미지가 깨지는 경우가 발생할 수 있습니다.</li>
                             <li>최대 지원 사이즈인 640 X 640으로 리사이즈 해서 올려주세요.(개당 이미지 최대 10M)</li>
                         </ul>
                     </div>
-                </li>
-                <li className={classes.label2}>
+                </div>
+                <div className={classes.label2}>
                     <div className={classes.labelTitle}>
                         제목
                     </div>
@@ -60,8 +74,8 @@ const AddProduct = () => {
                     <div>
                             {nameLength}/40
                     </div>
-                </li>
-                <li className={classes.label2}>
+                </div>
+                <div className={classes.label2}>
                     <label htmlFor="price" className={classes.labelTitle}>
                         가격
                     </label>
@@ -74,8 +88,8 @@ const AddProduct = () => {
                         required
                     />
                     <div>BB</div>
-                </li>
-                <li className={classes.label2}>
+                </div>
+                <div className={classes.label2}>
                     <div className={classes.labelTitle}>
                         카테고리
                     </div>
@@ -101,18 +115,18 @@ const AddProduct = () => {
                             <label><input type="checkbox" />기타</label>
                         </div>
                     </div>
-                </li>
-                <li className={classes.label}>
+                </div>
+                <div className={classes.label}>
                     <div className={classes.labelTitle}>
                         설명
                     </div>
                     <textarea className={classes.inputTextarea} />
-                </li>
-                <li className={classes.addButton}>
-                    <button type="submit" className={classes.button}>등록하기</button>
-                </li>
+                </div>
+                <div className={classes.addButton}>
+                    <button type="submit" className={classes.button} onSubmit={handleSubmit}>등록하기</button>
+                </div>
                 
-            </ul>
+            </form>
         </Fragment>
     );
 };
