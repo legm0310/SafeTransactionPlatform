c90c91945b814cc1be9feb04ccc30466be30a212
Lee gyu min <legm0310@gmail.com>
2023-05-07 08:19:41 +0900

    Refactor: 인증 미들웨어, passport추가

---
+++ server/config/passport.js
@@ -0,0 +1,60 @@
+const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
+const { Container } = require("typedi");
+const config = require("../config");
+const ApiError = require("../utils/ApiError");
+
+const cookieExtractor = function (req) {
+  var token = null;
+  if (req && req.cookies) {
+    token = req.cookies["refreshToken"];
+  }
+  return token;
+};
+
+const accessOptions = {
+  secretOrKey: config.jwtAccessSecret,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+};
+
+const refreshOptions = {
+  secretOrKey: config.jwtRefreshSecret,
+  jwtFromRequest: cookieExtractor,
+};
+
+const accessVerify = async (payload, done) => {
+  try {
+    console.log(payload);
+    return done(null, payload);
+  } catch (err) {
+    return done(err, false);
+  }
+};
+
+const refreshVerify = async (payload, done) => {
+  const tokenService = await Container.get("tokenService");
+  try {
+    const tokenData = await tokenService.getTokenByUserId(payload.sub);
+    const tokenValues = tokenData.dataValues;
+    const reissueTimeout = tokenValues.reissue_timeout.getTime();
+
+    console.log("재생성 만료기간", reissueTimeout / 1000);
+    console.log("현재 타임스탬프", Math.floor(Date.now() / 1000));
+
+    if (!reissueTimeout || reissueTimeout < Date.now()) {
+      throw new ApiError(401, "Unauthorized", "Please authenticate");
+    } else {
+      payload["tokenData"] = tokenValues;
+      return done(null, payload);
+    }
+  } catch (err) {
+    done(err, false);
+  }
+};
+
+const accessStrategy = new JwtStrategy(accessOptions, accessVerify);
+const refreshStrategy = new JwtStrategy(refreshOptions, refreshVerify);
+
+module.exports = {
+  accessStrategy,
+  refreshStrategy,
+};
--- server/loaders/express.js
+++ server/loaders/express.js
@@ -2,10 +2,35 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const passport = require("passport");
 const morgan = require("morgan");
 const routerLoader = require("../routes");
+const { accessStrategy, refreshStrategy } = require("../config/passport");
 
 module.exports = ({ app }) => {
+  //Http 요청, 응답 로깅
+  app.use(morgan("dev"));
+
+  //static 디렉터리 설정
+  app.use(express.static(path.join(__dirname, "public")));
+
+  //json, urlencoded request body / cookie parse
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
+
+  //cors
+  app.use(cors());
+
+  //jwt authentication
+  app.use(passport.initialize());
+  passport.use("access", accessStrategy);
+  passport.use("refresh", refreshStrategy);
+
+  //api routes
+  app.use("/api", routerLoader());
+
+  //test routes
   app.get("/reqCheck", (req, res) => {
     console.log(req.headers);
     console.log(req.cookies);
@@ -17,20 +42,18 @@ module.exports = ({ app }) => {
   app.head("/status", (req, res) => {
     res.status(200).end();
   });
-  app.use(cors());
-  app.use(morgan("dev"));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, "public")));
-  app.use("/", routerLoader());
 
+  //요청 자원(Url)이 없을 때
   app.use((req, res, next) => {
     const err = new Error("Not Found");
     err["status"] = 404;
     next(err);
   });
-  /// error handlers
+
+  /**  
+   * error handlers
+  @TODO refactoring
+  */
   app.use((err, req, res, next) => {
     if (err.name === "Unauthorized") {
       return res.status(401).send({ message: err.message }).end();
