5070b25901e8cc885c03fe0c189bb2de0e5dacca
legm0310 <legm0310@email.com>
2023-05-08 07:38:21 +0900

    Feat: product

--- server/services/index.js
+++ server/services/index.js
@@ -7,4 +7,5 @@ module.exports = {
   userService: UserService,
   tokenService: TokenService,
   authService: AuthService,
+  productService: ProductService,
 };
--- server/services/product.js
+++ server/services/product.js
@@ -1,3 +1,9 @@
-class ProductService {}
+const { Container } = require("typedi");
+
+class ProductService {
+  constructor() {
+    this.Product = Container.get("productModel");
+  }
+}
 
 module.exports = ProductService;
