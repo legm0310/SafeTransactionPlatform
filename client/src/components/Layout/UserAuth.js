140bc1f457a7fe0cbad545fae6087d9b8bbcd96a
LeeSeungHoon <ysh73900@gmail.com>
2023-05-08 07:32:41 +0900

    Refactor: onLogoutHandler 로직

--- client/src/components/Layout/UserAuth.js
+++ client/src/components/Layout/UserAuth.js
@@ -1,36 +1,35 @@
 import { Fragment } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 import classes from "./UserAuth.module.css";
 
-const navigate = useNavigate();
-
-const onLogoutHandler = () => {
-  axios.defaults.withCredentials = true;
-  axios
-    .get("/api/auth/logout", {
-      // 'withCredentials'속성을 'true'로 설정하여 요청을 보낼 때 쿠키에 토큰을 추가
-      withCredentials: true,
-    })
-    .then((response) => {
-      if (response.data.logoutSuccess) {
-        // localStorage 지우기
-        localStorage.clear();
-
-        // 쿠키 지우기
-        document.cookie =
-          "쿠키이름=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        alert("로그아웃 되었습니다.");
-        navigate("/");
-      } else {
-        alert("로그아웃에 실패 했습니다.");
-      }
-    });
-};
-
-const isLoggedIn = true;
-
 const UserAuth = (props) => {
+  const navigate = useNavigate();
+  const isLoggedIn = true;
+
+  const onLogoutHandler = () => {
+    axios.defaults.withCredentials = true;
+    axios
+      .get("/api/auth/logout", {
+        // 'withCredentials'속성을 'true'로 설정하여 요청을 보낼 때 쿠키에 토큰을 추가
+        withCredentials: true,
+      })
+      .then((response) => {
+        if (response.data.logoutSuccess) {
+          // localStorage 지우기
+          localStorage.clear();
+
+          // 쿠키 지우기
+          document.cookie =
+            "쿠키이름=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+          alert("로그아웃 되었습니다.");
+          navigate("/");
+        } else {
+          alert("로그아웃에 실패 했습니다.");
+        }
+      });
+  };
   return (
     <Fragment>
       <ul className={classes.Authwrap}>
