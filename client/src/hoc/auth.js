ff21a2127e4b466a59c6ceb108f2b1968051b943
LeeSeungHoon <ysh73900@gmail.com>
2023-05-08 05:53:34 +0900

    Refactor: 인증로직

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -34,9 +34,9 @@ export default function (SpecificComponent, option, adminRoute = null) {
 
       dispatch(auth()).then((response) => {
         console.log(response);
-        console.log(response.payload.isAuth);
+        console.log(response.payload.authCheckSuccess);
         // 로그인 하지 않은 상태
-        if (!response.payload.isAuth) {
+        if (!response.payload.authCheckSuccess) {
           // 로그인 하지 않은 상태에서 로그인한 유저만 출입 가능한 페이지로 들어가려고 할 때
           if (option) {
             navigate("/");
