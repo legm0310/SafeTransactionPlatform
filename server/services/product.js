483c90f7c7b9dc610446c278c153e15c74d87915
Lee gyu min <legm0310@gmail.com>
2023-05-09 09:08:14 +0900

    Feat: productService 메서드 구현

--- server/services/product.js
+++ server/services/product.js
@@ -4,6 +4,36 @@ class ProductService {
   constructor() {
     this.Product = Container.get("productModel");
   }
+
+  async addProduct() {}
+
+  async getProduct() {
+    try {
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async updateProduct() {
+    try {
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async deleteProduct() {
+    try {
+      const deletedRows = await User.destroy({
+        where: {},
+      });
+      if (!deletedRows) {
+        throw new Error("User not found");
+      }
+      return deletedRows;
+    } catch (err) {
+      throw err;
+    }
+  }
 }
 
 module.exports = ProductService;
