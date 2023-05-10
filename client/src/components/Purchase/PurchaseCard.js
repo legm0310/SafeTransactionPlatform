29773be8cf18141b15d0c361d22092199b6b082f
LeeSeungHoon <ysh73900@gmail.com>
2023-05-11 02:06:23 +0900

    Rename: PurchaseList->PurchaseCard

---
+++ client/src/components/Purchase/PurchaseCard.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from "react";
+
+import classes from "./PurchaseCard.module.css";
+
+const PurchaseCard = (props) => {
+  return (
+    <Fragment>
+      <div className={classes.prodCardWrap}>
+        <div className={classes.prodCardContainer}>
+          {props.products.map((product) => (
+            <div className={classes.prodCard}>
+              <div className={classes.imgBox}>
+                <img src={product.imgFile} className={classes.prodImg} />
+              </div>
+
+              <div className={classes.prodInfo}>
+                <div className={classes.prodName}>{product.name}</div>
+                <div className={classes.prodPrice}> {product.price}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default PurchaseCard;
