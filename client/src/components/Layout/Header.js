3f05e8e58ef211e76a032514ae1f38527fc94c23
LeeSeungHoon <ysh73900@gmail.com>
2023-05-24 01:31:55 +0900

    Refactor: className 및 jsx코드

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,7 +1,5 @@
 import { Fragment } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { auth } from "../../_actions/user_action";
+import { Link, useLocation } from "react-router-dom";
 
 import classes from "./Header.module.css";
 import { FiSearch } from "react-icons/fi";
@@ -10,8 +8,6 @@ import UserAuth from "./UserAuth";
 
 const Header = (props) => {
   const location = useLocation();
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
 
   // 현재 경로가 로그인 페이지인 경우 Header를 렌더링하지 않음
   if (location.pathname === "/login" || location.pathname === "/register") {
@@ -21,14 +17,31 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <div className={classes.list}>
-          <Link to="/" className={classes.link}>
+        <div className={classes.headerTop}>
+          <Link to="/" className={classes.logoWrap}>
             <h1 className={classes.logo}>PANDA.</h1>
           </Link>
 
-          <ul className={classes.list2}>
+          <div className={classes.searchBox}>
+            <input type="text" className={classes.search} />
+
+            <button>
+              <FiSearch />
+            </button>
+          </div>
+
+          <div className={classes.AuthList}>
+            <UserAuth
+              setIsLoggedIn={props.setIsLoggedIn}
+              isLoggedIn={props.isLoggedIn}
+            />
+          </div>
+        </div>
+
+        <div className={classes.headerBottom}>
+          <ul className={classes.list}>
             <div className={classes.dropdown}>
-              <li className={classes.list2Item}>
+              <li className={classes.purchaseList}>
                 <Link to="/purchase/" className={classes.purchaseLink}>
                   구매하기
                 </Link>
@@ -40,24 +53,9 @@ const Header = (props) => {
             </div>
 
             <Link to="/addProduct/" className={classes.AddProductLink}>
-              <li className={classes.list2Item}>판매하기</li>
+              <li className={classes.AddProductList}>판매하기</li>
             </Link>
           </ul>
-
-          <div className={classes.searchBox}>
-            <input type="text" className={classes.search} />
-
-            <button>
-              <FiSearch />
-            </button>
-          </div>
-        </div>
-
-        <div className={classes.AuthList}>
-          <UserAuth
-            setIsLoggedIn={props.setIsLoggedIn}
-            isLoggedIn={props.isLoggedIn}
-          />
         </div>
       </header>
     </Fragment>
