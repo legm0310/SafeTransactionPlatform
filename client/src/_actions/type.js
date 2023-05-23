c2fccad5034a293bbfed237bf4b460081e9b442a
LeeSeungHoon <ysh73900@gmail.com>
2023-05-24 01:11:55 +0900

    Feat: ADD_PRODUCT type

--- client/src/_actions/type.js
+++ client/src/_actions/type.js
@@ -10,3 +10,5 @@ export const SIGNUP_USER = "signup_user";
 export const LOGIN_USER = "login_user";
 export const LOGOUT_USER = "logout_user";
 export const AUTH_USER = "auth_user";
+
+export const ADD_PRODUCT = "add_product";
