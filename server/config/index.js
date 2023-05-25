eade86133ceaf7e781fe184f4fc547a96d877eb4
Lee gyu min <legm0310@gmail.com>
2023-05-26 02:42:08 +0900

    Comment: config 주석 추가

--- server/config/index.js
+++ server/config/index.js
@@ -16,36 +16,11 @@ if (envFound.error) {
 }
 
 module.exports = {
+  //server 구동 환경
   nodeEnv: process.env.NODE_ENV,
   port: parseInt(process.env.PORT, 10),
-  jwtAccessSecret: process.env.JWT_SECRET_ACCESS,
-  jwtRefreshSecret: process.env.JWT_SECRET_REFRESH,
-  jwtAlgorithm: null,
 
-  reissueTimeoutInterval: process.env.REISSUE_TIMEOUT_INTERVAL,
-
-  awsS3BucketName: process.env.AWS_S3_BUCKET_NAME,
-  awsS3Config: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
-  },
-
-  cors: {
-    origin: process.env.CORS_ORIGIN.split(","),
-    credentials: true,
-    exposedHeaders: ["Authorization"],
-  },
-
-  cookieSet: {
-    domain: process.env.DOMAIN,
-    path: "/",
-    maxAge: 24 * 6 * 60 * 10000,
-    sameSite: "none",
-    httpOnly: true,
-    secure: true,
-  },
-
+  //db 설정
   db: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
@@ -56,5 +31,37 @@ module.exports = {
     dialectOptions: {
       timezone: "+09:00", // DB에서 가져올 때 시간 설정
     },
+  },
+
+  //jwt auth token
+  jwtAccessSecret: process.env.JWT_SECRET_ACCESS,
+  jwtRefreshSecret: process.env.JWT_SECRET_REFRESH,
+  jwtAlgorithm: null,
+  //만료된 acToken으로 새 acToken을 발급 받을 수 있는 시간
+  reissueTimeoutInterval: process.env.REISSUE_TIMEOUT_INTERVAL,
+
+  //aws
+  awsS3BucketName: process.env.AWS_S3_BUCKET_NAME,
+  awsS3Config: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+  },
+
+  //cors (production 환경에서만 사용)
+  cors: {
+    origin: process.env.CORS_ORIGIN.split(","),
+    credentials: true,
+    exposedHeaders: ["Authorization"],
+  },
+
+  //cookie 발급 옵션
+  cookieSet: {
+    domain: process.env.DOMAIN,
+    path: "/",
+    maxAge: 24 * 6 * 60 * 10000,
+    sameSite: "none",
+    httpOnly: true,
+    secure: true,
   },
 };
