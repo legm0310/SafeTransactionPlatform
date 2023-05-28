43cd354d784f0ab94ee6d737f6a661cd2a77b14a
Lee gyu min <legm0310@gmail.com>
2023-05-29 08:04:04 +0900

    Chore: 테스트용 코드 제거

--- server/middlewares/uploader.js
+++ server/middlewares/uploader.js
@@ -1,7 +1,6 @@
 const { uploadProdImg } = require("../utils");
 
 const uploader = (req, res, next) => {
-  console.log(req.body);
   uploadProdImg.array("files", 10)(req, res, (error) => {
     if (error) {
       console.log(error);
