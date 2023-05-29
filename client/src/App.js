72ec292885a866a4c8b93f01dfacf074e5da011c
ysh73900 <kim73900@naver.com>
2023-05-29 22:52:47 +0900

    Refactor: 라우트 경로 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,27 @@
-import React, { Fragment, useState } from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import React, { Fragment, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Header from "./components/Layout/Header"
-import Home from "./components/Home/Home"
-import Purchase from "./components/Purchase/Purchase"
-import AddProduct from "./components/AddProduct/AddProduct"
-import Login from "./components/Auth/Login"
-import Register from "./components/Auth/Register"
-import Detail from "./components/Purchase/Detail"
-import Auth from "./hoc/auth"
+import Header from "./components/Layout/Header";
+import Home from "./components/Home/Home";
+import Purchase from "./components/Purchase/Purchase";
+import AddProduct from "./components/AddProduct/AddProduct";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import Detail from "./components/Purchase/Detail";
+import Auth from "./hoc/auth";
 
 function App() {
-  const AuthHome = Auth(Home, null)
-  const AuthAddProduct = Auth(AddProduct, true)
+  const AuthHome = Auth(Home, null);
+  const AuthAddProduct = Auth(AddProduct, true);
 
-  const token = localStorage.getItem("accessToken")
+  const token = localStorage.getItem("accessToken");
   const tokenCheck =
     token !== "undefined" || token !== "null" || token !== ""
       ? Boolean(token)
-      : false
+      : false;
 
-  const [purchaseCard, setPurchaseCard] = useState([])
-  const [isLoggedIn, setIsLoggedIn] = useState(tokenCheck)
+  const [purchaseCard, setPurchaseCard] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(tokenCheck);
 
   const addProductHandler = (pName, pPrice, pImg, pExplanation, pCategory) => {
     setPurchaseCard((prevPurchaseCard) => {
@@ -35,39 +35,39 @@ function App() {
           category: pCategory,
           id: Math.random().toString(),
         },
-      ]
-    })
-  }
+      ];
+    });
+  };
 
   return (
     <Fragment>
       <BrowserRouter>
         <Header setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} />
         <Routes>
-          <Route path='/' element={<AuthHome />}></Route>
+          <Route path="/" element={<AuthHome />}></Route>
           <Route
-            path='/purchase/'
+            path="/product/all"
             element={<Purchase purchaseCard={purchaseCard} />}
           ></Route>
           <Route
-            path='/addProduct/'
+            path="/product/add"
             element={<AuthAddProduct onAddProduct={addProductHandler} />}
           ></Route>
           <Route
-            path='/login'
+            path="/login"
             element={
               <Login setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} />
             }
           ></Route>
-          <Route path='/register' element={<Register />}></Route>
+          <Route path="/register" element={<Register />}></Route>
           <Route
-            path='/detail/:id'
+            path="/detail"
             element={<Detail purchaseCard={purchaseCard} />}
           ></Route>
         </Routes>
       </BrowserRouter>
     </Fragment>
-  )
+  );
 }
 
-export default App
+export default App;
