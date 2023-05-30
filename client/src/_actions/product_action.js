d5f04f7969cef57169489757f0e705d02be25dcd
Lee Gyumin <legm0310@gmail.com>
2023-05-30 15:54:22 +0900

    Feat: addProduct action 헤더 추가, upload 필드 수정
    Co-authored-by: LeeSeungHoon <ysh73900@gmail.com>

--- client/src/_actions/product_action.js
+++ client/src/_actions/product_action.js
@@ -1,9 +1,36 @@
 import axios from "axios";
 import { ADD_PRODUCT } from "./type";
 
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+axios.defaults.withCredentials = true;
+axios.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken && config.method === "post") {
+      config.headers["Authorization"] = accessToken;
+    }
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
+axios.interceptors.response.use(
+  (res) => {
+    console.log("hi", res.headers.authorization);
+    const newAccessToken = res.headers.authorization;
+    if (newAccessToken) {
+      localStorage.setItem("accessToken", newAccessToken);
+    }
+    return res;
+  },
+  (err) => Promise.reject(err)
+);
+
 export function addProduct(dataToSubmit) {
+  const accessToken = localStorage.getItem("accessToken");
   const headers = {
     "Content-Type": "multipart/form-data",
+    Authorization: accessToken,
+    "Cache-control": "no-cache, no-store",
   };
   const request = axios
     .post("/api/product", dataToSubmit, {
