3d4da3ea9bc4e3a54e2612dd1ada11435902d1fa
Lee Gyumin <legm0310@gmail.com>
2023-05-30 16:00:01 +0900

    Test: upload test추가

--- server/middlewares/uploader.js
+++ server/middlewares/uploader.js
@@ -1,14 +1,20 @@
 const { uploadProdImg } = require("../utils");
 
 const uploader = (req, res, next) => {
+  // console.log(123123123, err);
+  // if (err) {
+  //   console.log(2222, err);
+  //   return next(err);
+  // }
+  // console.log(123);
   uploadProdImg.fields([{ name: "product", maxCount: 10 }])(
     req,
     res,
     (error) => {
       if (error) {
-        next(error);
+        return next(error);
       }
-      next();
+      return next();
     }
   );
 };
