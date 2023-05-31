1b63aeeda5ddd3f5ee58e1f0e8feed3d5f5e3415
legm0310 <legm0310@email.com>
2023-05-31 18:40:21 +0900

    Fix: sequelize 로더 에러핸들링, isAuth 선언부 추가

--- server/loaders/sequelize.js
+++ server/loaders/sequelize.js
@@ -1,6 +1,7 @@
 const db = require("../models");
 module.exports = async () => {
   await db.sequelize.sync({ force: false }).catch((err) => {
-    console.error(err);
+    console.log(err);
+    process.exit(err.code);
   });
 };
