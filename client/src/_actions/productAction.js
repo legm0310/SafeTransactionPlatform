5fa9c3a812c15612cba127b885f6637adc004814
Lee Gyumin <legm0310@gmail.com>
2023-06-04 02:25:31 +0900

    Refactor: axios요청 모듈화
    Co-authored-by: ysh73900 <kim73900@naver.com>

--- client/src/_actions/productAction.js
+++ client/src/_actions/productAction.js
@@ -1,41 +1,10 @@
 import axios from "axios";
 import { ADD_PRODUCT } from "./type";
-
-axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
-axios.defaults.withCredentials = true;
-axios.interceptors.request.use(
-  (config) => {
-    const accessToken = localStorage.getItem("accessToken");
-    if (accessToken && config.method === "post") {
-      config.headers["Authorization"] = accessToken;
-    }
-    return config;
-  },
-  (err) => Promise.reject(err)
-);
-axios.interceptors.response.use(
-  (res) => {
-    console.log("hi", res.headers.authorization);
-    const newAccessToken = res.headers.authorization;
-    if (newAccessToken) {
-      localStorage.setItem("accessToken", newAccessToken);
-    }
-    return res;
-  },
-  (err) => Promise.reject(err)
-);
+import { addProdRequest } from "../api/productApi";
 
 export function addProduct(dataToSubmit) {
-  const accessToken = localStorage.getItem("accessToken");
-  const headers = {
-    "Content-Type": "multipart/form-data",
-    Authorization: accessToken,
-    "Cache-control": "no-cache, no-store",
-  };
-  const request = axios
-    .post("/api/product", dataToSubmit, {
-      headers,
-    })
+  const request = addProdRequest()
+    .post("/api/product", dataToSubmit)
     .then((response) => {
       console.log("res", response);
       return response.data;
