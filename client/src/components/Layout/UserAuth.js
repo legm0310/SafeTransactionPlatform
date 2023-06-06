3dc6bf4fe640b9f18d39ef9df8ab6efdaa1ab288
junhyeon0218 <junhyeon0218@daum.net>
2023-06-06 23:57:22 +0900

    Feat: 로그인 후 상태 컴포넌트로 분할

--- client/src/components/Layout/UserAuth.js
+++ client/src/components/Layout/UserAuth.js
@@ -1,31 +1,17 @@
-import { Fragment } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
-import { logout } from "../../_actions/userAction";
-
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import UserSection from "./UserSection";
 import classes from "../../styles/UserAuth.module.css";
 
 const UserAuth = (props) => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
-  const onLogoutHandler = () => {
-    dispatch(logout()).then((response) => {
-      alert("로그아웃 되었습니다.");
-      navigate("/");
-    });
-  };
 
   return (
-    <Fragment>
+    <div>
       <div className={classes.Authwrap}>
         {isLoggedIn ? (
-          <div>
-            <div> 내 지갑 관리 </div>
-            <div className={classes.logout} onClick={onLogoutHandler}>
-              로그아웃
-            </div>
-          </div>
+          <UserSection />
         ) : (
           <Link to='/login' className={classes.loginRegister}>
             <div className={classes.login}>로그인/</div>
@@ -33,7 +19,7 @@ const UserAuth = (props) => {
           </Link>
         )}
       </div>
-    </Fragment>
+    </div>
   );
 };
 
