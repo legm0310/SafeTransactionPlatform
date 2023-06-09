d6ce40b422c98fa901ca599023893d1344d8f884
LeeSeungHoon <ysh73900@gmail.com>
2023-06-10 03:05:44 +0900

    Refactor: 사용하지않는 함수 삭제 및 Link 삭제

--- client/src/pages/Product/Product.js
+++ client/src/pages/Product/Product.js
@@ -1,12 +1,8 @@
 import React, { Fragment, useState, useEffect } from "react";
 import classes from "../../styles/Product.module.css";
-import ProductCard from "./ProductCard";
 import RecentProductsList from "../../components/RecentProductsList";
-import { Link, useNavigate } from "react-router-dom";
 
 const Product = (props) => {
-  const navigate = useNavigate();
-
   const [selectedCategory, setSelectedCategory] = useState("all");
   const [filteredProducts, setFilteredProducts] = useState([]);
 
@@ -27,39 +23,32 @@ const Product = (props) => {
               card.productCategory.includes(selectedCategory)
           );
     setFilteredProducts(filtered);
-  }, [selectedCategory]);
+  }, [selectedCategory, props.ProductCard]);
 
   return (
     <Fragment>
       <div className={classes.mainBox}>
-        <Link to="/detail" className={classes.detail}>
-          <div className={classes.categoryBox}>
-            <div onClick={() => onCategoryClick("all")}>전체</div>
-            <ul className={classes.category1}>
-              <li onClick={() => onCategoryClick("men")}>남성의류</li>
-              <li onClick={() => onCategoryClick("women")}>여성의류</li>
-              <li onClick={() => onCategoryClick("acc")}>패션잡화</li>
-              <li onClick={() => onCategoryClick("sports")}>스포츠 용품</li>
-              <li onClick={() => onCategoryClick("shoes")}>신발</li>
-            </ul>
-            <ul className={classes.category2}>
-              <li onClick={() => onCategoryClick("homeappliances")}>
-                가전제품
-              </li>
-              <li onClick={() => onCategoryClick("computerPeripherals")}>
-                컴퓨터/주변기기
-              </li>
-              <li onClick={() => onCategoryClick("electronic")}>전자제품</li>
-              <li onClick={() => onCategoryClick("furniture")}>가구</li>
-              <li onClick={() => onCategoryClick("etc")}>기타</li>
-            </ul>
-          </div>
-        </Link>
+        <div className={classes.categoryBox}>
+          <div onClick={() => onCategoryClick("all")}>전체</div>
+          <ul className={classes.category1}>
+            <li onClick={() => onCategoryClick("men")}>남성의류</li>
+            <li onClick={() => onCategoryClick("women")}>여성의류</li>
+            <li onClick={() => onCategoryClick("acc")}>패션잡화</li>
+            <li onClick={() => onCategoryClick("sports")}>스포츠 용품</li>
+            <li onClick={() => onCategoryClick("shoes")}>신발</li>
+          </ul>
+          <ul className={classes.category2}>
+            <li onClick={() => onCategoryClick("homeappliances")}>가전제품</li>
+            <li onClick={() => onCategoryClick("computerPeripherals")}>
+              컴퓨터/주변기기
+            </li>
+            <li onClick={() => onCategoryClick("electronic")}>전자제품</li>
+            <li onClick={() => onCategoryClick("furniture")}>가구</li>
+            <li onClick={() => onCategoryClick("etc")}>기타</li>
+          </ul>
+        </div>
         <div className={classes.productWrap}>
-          <Link to="/detail" className={classes.detail}>
-            <RecentProductsList />
-            {/* <ProductCard products={filteredProducts} /> */}
-          </Link>
+          <RecentProductsList />
         </div>
       </div>
     </Fragment>
