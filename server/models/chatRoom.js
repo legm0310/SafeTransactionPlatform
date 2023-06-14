0066e198dd58aad160f8d240fffd348c401d37e1
cbjoon <qudwns017@naver.com>
2023-06-15 04:28:54 +0900

    Feat: chatParticipant, chatRoom, models/index 수정

--- server/models/chatRoom.js
+++ server/models/chatRoom.js
@@ -4,12 +4,40 @@ class ChatRoom extends Sequelize.Model {
   static init(sequelize) {
     super.init(
       {
-        //...
+        chatRoomId: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+        },
+        userId: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+        },
+        userId2: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+        },
+        roomNum: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+        },
       },
       {
+        modelName: "chatRoom", // This is the name of the table in the database
+        freezeTableName: true,
+        timestamps: true,
+        underscored: true,
         sequelize,
       }
     );
+  }
+  static associate(db) {
+    ChatRoom.hasMany(db.ChatParticipant, { foreignKey: "chatRoomId" });
+    ChatRoom.belongsTo(db.User, {
+      foreignKey: "userId",
+      onDelete: "cascade",
+    });
   }
 }
 
