5764c7a223a407f78940e01cf8e90b357f837b75
junhyeon0218 <junhyeon0218@daum.net>
2023-06-14 23:34:49 +0900

    Feat: MUI로 깔끔해

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -3,10 +3,26 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { signup } from "../../_actions/userAction";
-import Button from "../../components/UI/Button";
+// import Button from "../../components/UI/Button";
 
 import classes from "../../styles/Register.module.css";
 import { FaArrowLeft } from "react-icons/fa";
+
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  FormControl,
+  FormControlLabel,
+  Checkbox,
+  FormHelperText,
+  Grid,
+  Box,
+  Typography,
+  Container,
+} from "@mui/material/";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const Register = (props) => {
   const dispatch = useDispatch();
@@ -18,33 +34,81 @@ const Register = (props) => {
   const [Password, setPassword] = useState("");
   const [ConfirmPassword, setConfirmPassword] = useState("");
 
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [phoneNumberError, setPhoneNumberError] = useState("");
+  const [registerError, setRegisterError] = useState("");
+
   const onEmailHandler = (event) => {
     setEmail(event.currentTarget.value);
+
+    // 이메일 유효성 체크
+    const emailPattern = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    const isValidEmail = emailPattern.test(event.currentTarget.value);
+    if (!isValidEmail) {
+      setEmailError("유효한 이메일 주소를 입력해주세요.");
+    } else {
+      setEmailError("");
+    }
   };
 
   const onNameHandler = (event) => {
     setName(event.currentTarget.value);
+
+    // 이름 유효성 체크
+    if (event.currentTarget.value.trim() === "") {
+      setNameError("이름을 입력해주세요.");
+    } else {
+      setNameError("");
+    }
   };
 
   const onPhoneNumberHandler = (event) => {
     setPhoneNumber(event.currentTarget.value);
+
+    // 핸드폰 번호 유효성 체크
+    const phoneNumberPattern = /^\d{3}-\d{3,4}-\d{4}$/;
+    const isValidPhoneNumber = phoneNumberPattern.test(
+      event.currentTarget.value
+    );
+    if (!isValidPhoneNumber) {
+      setPhoneNumberError("유효한 핸드폰 번호를 입력해주세요.");
+    } else {
+      setPhoneNumberError("");
+    }
   };
 
   const onPasswordHandler = (event) => {
     setPassword(event.currentTarget.value);
+
+    // 비밀번호 유효성 체크
+    const passwordPattern = /^(?=.*\d)(?=.*[a-zA-Z])[a-zA-Z0-9]{8,}$/;
+    const isValidPassword = passwordPattern.test(event.currentTarget.value);
+    if (!isValidPassword) {
+      setPasswordError(
+        "비밀번호는 숫자와 영문자를 조합하여 8자 이상으로 입력해주세요."
+      );
+    } else {
+      setPasswordError("");
+    }
   };
 
   const onConfirmPasswordHandler = (event) => {
     setConfirmPassword(event.currentTarget.value);
+
+    // 비밀번호 재입력 체크
+    if (event.currentTarget.value !== Password) {
+      setPasswordError("비밀번호가 일치하지 않습니다.");
+    } else {
+      setPasswordError("");
+    }
   };
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
 
-    if (Password !== ConfirmPassword) {
-      return alert("비밀번호가 같지 않습니다.");
-    }
-
+    // 회원가입 요청 보내기
     let body = {
       role: 1,
       email: Email,
@@ -55,12 +119,14 @@ const Register = (props) => {
 
     dispatch(signup(body)).then((response) => {
       if (response.payload.signupSuccess) {
-        alert("회원 정보 입력 완료");
+        console.log("회원가입 성공");
         navigate("/login");
       } else if (response.payload.code === 400) {
         alert("존재하는 이메일입니다.");
+        setRegisterError("이미 가입된 이메일입니다.");
       } else {
         alert("회원 가입에 실패했습니다.");
+        setRegisterError("회원가입에 실패하였습니다. 다시 한번 확인해 주세요.");
       }
     });
   };
@@ -70,7 +136,7 @@ const Register = (props) => {
       <div className={classes.wrap}>
         <div className={classes.container}>
           <header className={classes.header}>
-            <Link to="/login" className={classes.backButton}>
+            <Link to='/login' className={classes.backButton}>
               <FaArrowLeft />
             </Link>
           </header>
@@ -84,73 +150,85 @@ const Register = (props) => {
             </div>
           </div>
 
-          <div className={classes.registerInputWrap}>
-            <form onSubmit={onSubmitHandler}>
-              <div className={classes.nameField}>
-                <div className={classes.nameInputGroup}>
-                  <input
-                    type="text"
-                    placeholder="이름"
+          <Box component='form' noValidate sx={{ px: 3 }}>
+            <FormControl
+              component='fieldset'
+              variant='standard'
+              onSubmit={onSubmitHandler}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    autoFocus
+                    fullWidth
+                    type='name'
                     value={Name}
+                    label='이름'
                     onChange={onNameHandler}
-                    className={classes.nameInput}
+                    error={nameError !== ""}
+                    helperText={nameError}
                   />
-                </div>
-              </div>
-
-              <div className={classes.idField}>
-                <div className={classes.idInputGroup}>
-                  <input
-                    type="email"
-                    placeholder="이메일"
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    autoFocus
+                    fullWidth
+                    type='email'
                     value={Email}
+                    label='이메일 주소'
                     onChange={onEmailHandler}
-                    className={classes.idInput}
+                    error={emailError !== ""}
+                    helperText={emailError}
                   />
-                </div>
-              </div>
-
-              <div className={classes.pnField}>
-                <div className={classes.pnInputGroup}>
-                  <input
-                    type="text"
-                    placeholder="핸드폰번호"
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    autoFocus
+                    fullWidth
+                    type='number'
                     value={PhoneNumber}
+                    label='핸드폰 번호'
                     onChange={onPhoneNumberHandler}
-                    className={classes.pnInput}
                   />
-                </div>
-              </div>
-
-              <div className={classes.pwField}>
-                <div className={classes.pwInputGroup}>
-                  <input
-                    type="password"
-                    placeholder="비밀번호"
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    type='password'
                     value={Password}
+                    label='비밀번호 (숫자+영문자)'
                     onChange={onPasswordHandler}
-                    className={classes.pwInput}
+                    error={passwordError !== ""}
+                    helperText={passwordError}
                   />
-                </div>
-              </div>
-
-              <div className={classes.pwField}>
-                <div className={classes.pwInputGroup}>
-                  <input
-                    type="password"
-                    placeholder="비밀번호 확인"
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    type='password'
                     value={ConfirmPassword}
+                    label='비밀번호 재입력'
                     onChange={onConfirmPasswordHandler}
-                    className={classes.pwInput}
+                    error={passwordError !== "" || false}
                   />
-                </div>
-              </div>
-
-              <Button>
-                <div className={classes.registerButton}>회원가입</div>
+                </Grid>
+              </Grid>
+              <Button
+                type='submit'
+                fullWidth
+                variant='contained'
+                sx={{ mt: 3, mb: 2 }}
+                size='large'
+              >
+                회원가입
               </Button>
-            </form>
-          </div>
+            </FormControl>
+          </Box>
         </div>
       </div>
     </Fragment>
