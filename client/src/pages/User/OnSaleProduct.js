3c8994d4435c61750cf0c9db8f081b2332c4e281
LeeSeungHoon <ysh73900@gmail.com>
2023-06-18 23:25:29 +0900

    Rename: 파일이름변경

---
+++ client/src/pages/User/OnSaleProduct.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const OnSaleProduct = () => {
+  return (
+    <Fragment>
+      <h1>판매상품</h1>
+    </Fragment>
+  );
+};
+
+export default OnSaleProduct;
---
+++ client/src/pages/User/ReservedProduct.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const ReservedProduct = () => {
+  return (
+    <Fragment>
+      <h1>구매진행상품</h1>
+    </Fragment>
+  );
+};
+
+export default ReservedProduct;
