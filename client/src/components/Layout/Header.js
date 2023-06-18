9648fb3ddb174009b02a9db9563a5c8601468119
Lee Gyumin <legm0310@gmail.com>
2023-06-18 16:55:10 +0900

    Chore: 스피너 라이브러리 추가

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,15 +1,19 @@
 import React, { Fragment, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 import UserAuth from "./UserAuth";
 import MobileNavMenu from "./MobileNavMenu";
 import DesktopNavMenu from "./DesktopNavMenu";
 import Logo from "./Logo";
+import Modal from "../UI/Modal";
+import ContractLoading from "../UI/ContractLoading";
 
 import classes from "../../styles/Header.module.css";
 import { AppBar, Container, Toolbar } from "@mui/material";
 
 const Header = (props) => {
   const location = useLocation();
+  // const isContactLoading = useSelector(state => state.ui.isContactLoading);
   const [anchorElNav, setAnchorElNav] = useState(null);
 
   // 현재 경로가 로그인 페이지인 경우 Header를 렌더링하지 않음
@@ -48,6 +52,9 @@ const Header = (props) => {
           </Toolbar>
         </Container>
       </AppBar>
+      {/* <Modal>
+        <ContractLoading />
+      </Modal> */}
     </Fragment>
   );
 };
