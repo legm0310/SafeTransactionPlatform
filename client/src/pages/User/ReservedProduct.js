9d32de82e15960a95dabafd8f62fe04495eb2b87
LeeSeungHoon <ysh73900@gmail.com>
2023-06-19 06:28:14 +0900

    Feat: thirdweb hook 사용

--- client/src/pages/User/ReservedProduct.js
+++ client/src/pages/User/ReservedProduct.js
@@ -1,6 +1,16 @@
 import { Fragment } from "react";
+import {
+  useContractEvents,
+  useContract,
+  useAddress,
+} from "@thirdweb-dev/react";
+
+const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
 
 const ReservedProduct = () => {
+  const { contract } = useContract(contractAddress);
+  const address = useAddress();
+
   return (
     <Fragment>
       <h1>구매진행상품</h1>
