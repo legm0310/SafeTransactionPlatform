5be79814aee994de49a65f940df458714157bbc5
legm0310 <legm0310@email.com>
2023-06-19 08:24:56 +0900

    Fix: admin user_name 변경

--- server/loaders/sequelize.js
+++ server/loaders/sequelize.js
@@ -7,35 +7,35 @@ module.exports = async () => {
       if (force) {
         db.User.create({
           role: 0,
-          user_name: "t",
+          user_name: "admin",
           email: "t@t.com",
           phone_number: "12312341234",
           password: "1234",
         });
         db.User.create({
           role: 0,
-          user_name: "a",
+          user_name: "adminA",
           email: "a@a.com",
           phone_number: "23423452345",
           password: "1234",
         });
         db.User.create({
           role: 0,
-          user_name: "b",
+          user_name: "adminB",
           email: "b@b.com",
           phone_number: "34534563456",
           password: "1234",
         });
         db.User.create({
           role: 0,
-          user_name: "c",
+          user_name: "adminC",
           email: "c@c.com",
           phone_number: "45645674567",
           password: "1234",
         });
         db.User.create({
           role: 0,
-          user_name: "d",
+          user_name: "adminD",
           email: "d@d.com",
           phone_number: "56756785678",
           password: "1234",
