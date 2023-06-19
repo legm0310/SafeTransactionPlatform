3ddd3db823d1063b29b06bf5a6fe5c6c26784482
qudwns017 <qudwns017@naver.com>
2023-06-19 22:12:26 +0900

    Feat: reducers index에 chatReducer 추가

--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import uiReducer from "./uiReducer";
 import userReducer from "./userReducer";
 import productReducer from "./productReducer";
+import chatReducer from "./chatReducer";
 
 const rootReducer = combineReducers({
   ui: uiReducer,
   user: userReducer,
   product: productReducer,
+  chat: chatReducer,
 });
 
 export default rootReducer;
