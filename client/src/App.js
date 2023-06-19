60389930322ac86be90438164919899f51d13400
junhyeon0218 <junhyeon0218@daum.net>
2023-06-20 05:54:11 +0900

    Feat: MUI Focus시 색상 테마 추가

--- client/src/App.js
+++ client/src/App.js
@@ -16,12 +16,60 @@ import Loading from "./components/UI/Loading";
 import ScrollTop from "./components/UI/ScrollTop";
 import UserInfo from "./pages/User/UserInfo";
 
-import { Backdrop } from "@mui/material";
+import {
+  createTheme,
+  ThemeProvider,
+  useTheme,
+  outlinedInputClasses,
+  Backdrop,
+} from "@mui/material/";
+
+// 테마: focus일때 색상 변경때문에 필요
+const customTheme = (outerTheme) =>
+  createTheme({
+    typography: {
+      fontFamily: "GongGothicMedium",
+    },
+    palette: {
+      mode: outerTheme.palette.mode,
+    },
+    components: {
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            "--TextField-brandBorderColor": "#E0E3E7",
+            "--TextField-brandBorderHoverColor": "#B2BAC2",
+            "--TextField-brandBorderFocusedColor": "#1ecfba",
+            "& label.Mui-focused": {
+              color: "var(--TextField-brandBorderFocusedColor)",
+            },
+          },
+        },
+      },
+      MuiOutlinedInput: {
+        styleOverrides: {
+          notchedOutline: {
+            borderColor: "var(--TextField-brandBorderColor)",
+          },
+          root: {
+            [`&:hover .${outlinedInputClasses.notchedOutline}`]: {
+              borderColor: "var(--TextField-brandBorderHoverColor)",
+            },
+            [`&.Mui-focused .${outlinedInputClasses.notchedOutline}`]: {
+              borderColor: "var(--TextField-brandBorderFocusedColor)",
+            },
+          },
+        },
+      },
+    },
+  });
 
 function App() {
   const dispatch = useDispatch();
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
   const isLoading = useSelector((state) => state.ui.isLoading);
+
+  const outerTheme = useTheme();
 
   useEffect(() => {
     dispatch(auth());
@@ -32,31 +80,33 @@ function App() {
 
   return (
     <Fragment>
-      {isLoggedIn !== null ? (
-        <BrowserRouter>
-          <ScrollTop />
-          <Backdrop open={isLoading}>
-            <Loading />
-          </Backdrop>
-          <Header />
-          <Routes>
-            <Route path="/" element={<AuthHome />}></Route>
-            <Route path="/products/all" element={<Product />}></Route>
-            <Route path="/products/add" element={<AuthAddProduct />}></Route>
-            <Route path="/chat" element={<Chat />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/register" element={<Register />}></Route>
-            <Route path="/products/:productId" element={<Detail />}></Route>
-            <Route path="/userInfo" element={<UserInfo />}></Route>
+      <ThemeProvider theme={customTheme(outerTheme)}>
+        {isLoggedIn !== null ? (
+          <BrowserRouter>
+            <ScrollTop />
+            <Backdrop open={isLoading}>
+              <Loading />
+            </Backdrop>
+            <Header />
+            <Routes>
+              <Route path='/' element={<AuthHome />}></Route>
+              <Route path='/products/all' element={<Product />}></Route>
+              <Route path='/products/add' element={<AuthAddProduct />}></Route>
+              <Route path='/chat' element={<Chat />}></Route>
+              <Route path='/login' element={<Login />}></Route>
+              <Route path='/register' element={<Register />}></Route>
+              <Route path='/products/:productId' element={<Detail />}></Route>
+              <Route path='/userInfo' element={<UserInfo />}></Route>
 
-            <Route path="/Loading" element={<Loading />}></Route>
-          </Routes>
-        </BrowserRouter>
-      ) : (
-        <div>
-          <Loading />
-        </div>
-      )}
+              <Route path='/Loading' element={<Loading />}></Route>
+            </Routes>
+          </BrowserRouter>
+        ) : (
+          <div>
+            <Loading />
+          </div>
+        )}
+      </ThemeProvider>
     </Fragment>
   );
 }
