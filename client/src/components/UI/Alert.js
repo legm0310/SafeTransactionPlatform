0a38f8d845e417e17132cb6c475847dccc412164
ysh73900 <kim73900@naver.com>
2023-06-29 17:20:05 +0900

    Rename: 파일이동으로 인한 삭제

--- client/src/components/UI/Alert.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Alert from "@mui/material/Alert";
-
-const CustomAlert = ({ severity, message }) => {
-  return <Alert severity={severity}>{message}</Alert>;
-};
-
-export default CustomAlert;
