0a0cf8617107b2115f41c73c46b78888f1834f7a
LeeSeungHoon <ysh73900@gmail.com>
2023-08-15 15:32:31 +0900

    merge

--- client/src/pages/Chat/Chat.js
+++ client/src/pages/Chat/Chat.js
@@ -3,10 +3,18 @@ import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
 import classes from "../../styles/chat/Chat.module.css";
+
 const socket = io.connect("http://localhost:5000");
 
 const Chat = (props) => {
-  return <Fragment>Chat</Fragment>;
+  return (
+    <Fragment>
+      {/* <div className={classes.talkWrap}>
+        <div className={classes.entireChatRoom}>chat</div>
+        <div className={classes.oneChatRoom}>chat1</div>
+      </div> */}
+    </Fragment>
+  );
 };
 
 export default Chat;
