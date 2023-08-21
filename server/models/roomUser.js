12cb61b8e40882267beb0134ceacdfac93ddc890
Lee Gyumin <legm0310@gmail.com>
2023-08-21 23:59:17 +0900

    Feat: 채팅방 유저 모델

---
+++ server/models/roomUser.js
@@ -0,0 +1,27 @@
+const Sequelize = require("sequelize");
+
+class RoomUsers extends Sequelize.Model {
+  static init(sequelize) {
+    super.init(
+      {
+        selfGranted: {
+          type: Sequelize.DataTypes.INTEGER.UNSIGNED,
+          allowNull: true,
+          comment:
+            "채팅방 나갔는지 판단 (null or 0: 둘 다 접근 가능, 유저id: 해당 유저만 접근 가능 )",
+          defaultValue: 0,
+        },
+      },
+      {
+        sequelize,
+        modelName: "room_users",
+        freezeTableName: true,
+        timestamps: false,
+        underscored: true,
+      }
+    );
+  }
+  static associate(db) {}
+}
+
+module.exports = RoomUsers;
