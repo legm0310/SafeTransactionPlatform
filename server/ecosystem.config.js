3e6002533d10b65635653f0d8a9f3e25ab29359c
Lee Gyumin <legm0310@gmail.com>
2023-08-27 23:07:16 +0900

    Feat: pm2 무중단 배포, 설정 파일 추가

---
+++ server/ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps: [
+    {
+      script: "app.js",
+
+      watch: ["services", "controllers"],
+      ignore_watch: ["node_modules"],
+
+      env: {},
+
+      output: "~/logs/pm2/console.log", // 로그 출력 경로 재설정
+      error: "~/logs/pm2/onsoleError.log", // 에러 로그 출력 경로 재설정
+    },
+  ],
+
+  deploy: {
+    production: {
+      // user: "SSH_USERNAME",
+      // host: "SSH_HOSTMACHINE",
+      // ref: "origin/master",
+      // repo: "GIT_REPOSITORY",
+      // path: "DESTINATION_PATH",
+      // "pre-deploy-local": "",
+      // "post-deploy":
+      //   "npm install && pm2 reload ecosystem.config.js --env production",
+      // "pre-setup": "",
+    },
+  },
+};
