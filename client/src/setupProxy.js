a6b3631cdc263fd3d292a0ee4344104409e816aa
LeeSeungHoon <ysh73900@gmail.com>
2023-09-01 17:24:49 +0900

    Fix: 서버 주소 변경

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://127.0.0.1:5555",
+      target: "http://127.0.0.1:5000",
       chagneOrigin: true,
     })
   );
