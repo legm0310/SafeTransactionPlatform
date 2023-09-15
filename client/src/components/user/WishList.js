b96ed3c6c540767859f8ec848f101ccbc647af1b
LeeSeungHoon <ysh73900@gmail.com>
2023-09-15 18:46:02 +0900

    Feat: 찜목록리스트

--- client/src/components/user/WishList.js
+++ client/src/components/user/WishList.js
@@ -1,30 +1,43 @@
 import { Fragment } from "react";
 import classes from "../../styles/user/WishList.module.css";
 import testImg from "../../assets/test.jpg";
+import deleteBtn from "../../assets/icon-delete.svg";
 
-const WishList = () => {
+const WishList = ({ wish }) => {
   return (
     <Fragment>
-      <header className={classes.wishListWrap}>
+      {/* <header className={classes.wishListWrap}>
         <div className={classes.tabTitle}>
           <input type="checkbox" />
           <span>상품정보</span>
           <span>상품금액</span>
           <span>주문하기</span>
         </div>
-      </header>
+      </header> */}
 
       <section className={classes.wishList}>
         <input type="checkbox" />
         <div className={classes.wishListProductWrap}>
           <div className={classes.wishListProductImage}>
-            <img src={testImg} alt="" />
+            <img src={wish?.image} alt="" />
           </div>
 
           <div className={classes.wishListProductInfo}>
-            <p>전자제품</p>
-            <p>컴퓨터</p>
-            <p>1000PDT</p>
+            <p className={classes.productCategory}>{wish?.category}</p>
+            <p className={classes.productName}>{wish?.name}</p>
+          </div>
+
+          <div className={classes.productPrice}>
+            <p>{wish?.price}PDT</p>
+          </div>
+
+          <div className={classes.wishListProductPurchase}>
+            <p className={classes.totalPrice}></p>
+            <button className={classes.btnSubmit}>구매하기</button>
+          </div>
+
+          <div className={classes.wishListProductRemove}>
+            <img src={deleteBtn} />
           </div>
         </div>
       </section>
