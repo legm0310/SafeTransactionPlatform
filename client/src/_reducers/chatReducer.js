1c8b094e5fd579ea664fd9d2c1d670916b907502
Lee Gyumin <legm0310@gmail.com>
2023-09-22 06:00:54 +0900

    Feat: chat 관련 action함수, 리듀서 생성

--- client/src/_reducers/chatReducer.js
+++ client/src/_reducers/chatReducer.js
@@ -1,6 +1,9 @@
-import { ADD_MESSAGE, ADD_ROOM, GET_ROOM } from "../_actions/type";
+import { ADD_MESSAGE, ADD_ROOM, GET_ROOMS, GET_CHATS } from "../_actions/type";
 
-const initialState = {};
+const initialState = {
+  rooms: [],
+  chats: [],
+};
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default function (state = initialState, action) {
@@ -8,13 +11,17 @@ export default function (state = initialState, action) {
     case ADD_ROOM:
       return { ...state, addRoomSuccess: action.payload };
       break;
-    case GET_ROOM:
-      return { ...state, getRoomSuccess: action.payload };
+    case GET_ROOMS: {
+      state.rooms = action.payload.rooms;
+      return { ...state, getRoomsSuccess: action.payload.getRoomsSuccess };
+      break;
+    }
+    case GET_CHATS:
+      return { ...state, getChatsSuccess: action.payload };
       break;
     case ADD_MESSAGE:
       return { ...state, addMessageSuccess: action.payload };
       break;
-
     default:
       return state;
   }
