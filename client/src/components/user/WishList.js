bbe5e31a649ab93a1fe1803ee1ee60f07a92ee2a
Lee Gyumin <legm0310@gmail.com>
2023-09-23 04:14:02 +0900

    Fix: redux store 초기값 설정, useSelector실제 존재 프로퍼티로 변경

--- client/src/components/user/WishList.js
+++ client/src/components/user/WishList.js
@@ -8,28 +8,27 @@ import classes from "../../styles/user/WishList.module.css";
 
 const WishList = ({ wish }) => {
   const dispatch = useDispatch();
-
   const userId = useSelector((state) => state.user.userId);
+  const { loadWishList } = useSelector((state) => state.user);
 
-  const { wishList } = useSelector((state) => state.user.getWishListSuccess);
   useEffect(() => {
     dispatch(getWishList(userId));
   }, []);
 
-  console.log(wishList);
+  console.log(loadWishList);
 
   console.log("User id = ", userId);
 
-  console.log(wish);
   return (
     <Fragment>
-      {wishList.length === 0 ? (
+      {loadWishList?.length === 0 ? (
         <div className={classes.notWishList}>
           <h2>찜목록에 담긴 상품이 없습니다.</h2>
           <p>원하는 상품을 찜목록에 담아보세요!</p>
         </div>
       ) : (
-        wishList.map((wishItem) => {
+        loadWishList?.map((wishItem) => {
+          console.log(wishItem.id);
           return (
             <WishItem
               key={`key-${wishItem?.id}`}
