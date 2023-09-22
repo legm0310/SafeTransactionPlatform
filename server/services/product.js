2963295ed62b98bfea8dd2eedbb4a5eeba5a5c4c
legm0310 <legm0310@email.com>
2023-09-22 20:41:29 +0900

    Chore: console.log출력 주석처리

--- server/services/product.js
+++ server/services/product.js
@@ -22,8 +22,8 @@ class ProductService {
     const query = generateGetProductsQuery(params);
     const { count, rows } = await this.Product.findAndCountAll(query);
     const pages = Math.ceil(count / 12);
-    console.log("query", query);
-    console.log(rows);
+    // console.log("query", query);
+    // console.log(rows);
     const extractedList = extractProductsList(rows);
     return { pages, prodList: extractedList };
   }
@@ -31,7 +31,7 @@ class ProductService {
   //infinite scrolling 방식 (lastId)
   async getProducts(params) {
     const query = generateGetProductsQuery(params);
-    console.log("query", query);
+    // console.log("query", query);
     const products = await this.Product.findAll(query);
     if (!products) throw new InternalServerError("Internal Server Error");
 
@@ -108,6 +108,5 @@ class ProductService {
     return wishList;
   }
 }
-
 
 module.exports = ProductService;
