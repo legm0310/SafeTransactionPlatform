c790848808feac795fd4dc77f5417c69af9a54bd
LeeSeungHoon <ysh73900@gmail.com>
2023-09-22 16:18:45 +0900

    Design: 내정보페이지 디자인

--- client/src/components/user/OnSaleProduct.js
+++ client/src/components/user/OnSaleProduct.js
@@ -1,9 +1,13 @@
 import { Fragment } from "react";
+import classes from "../../styles/user/OnSaleProduct.module.css";
 
 const OnSaleProduct = () => {
   return (
     <Fragment>
-      <h1>판매상품</h1>
+      <div className={classes.notSaleList}>
+        <h2>판매중인 상품이 없습니다.</h2>
+        <p>의미있는 상품을 판매해보세요!</p>
+      </div>
     </Fragment>
   );
 };
