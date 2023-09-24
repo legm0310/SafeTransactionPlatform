a99587ea734adbcf3519be2d115e6d42774def6a
qudwns017 <qudwns017@naver.com>
2023-09-25 06:00:48 +0900

    Feat: ProductCard API 연동

--- client/src/pages/Product/ProductCard.js
+++ client/src/pages/Product/ProductCard.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 import { useSelector } from "react-redux";
 import Paging from "../../components/common/Paging";
@@ -6,29 +7,26 @@ import Paging from "../../components/common/Paging";
 import classes from "../../styles/product/ProductCard.module.css";
 
 const ProductCard = ({ filteredProducts }) => {
-  // const searchedProducts = useSelector(
-  //   (state) => state.product.searchProducts?.products
-  // );
-  console.log(filteredProducts);
+  console.log("card", filteredProducts);
   return (
     <Fragment>
       <div className={classes.prodCardWrap}>
         <div className={classes.prodCardContainer}>
           {filteredProducts?.map((product) => (
-            <div className={classes.prodCard}>
-              <div className={classes.imgBox}>
-                <img src={product.image} className={classes.prodImg} alt="" />
+            <Link to={`/products/${product.id}`}>
+              <div className={classes.prodCard}>
+                <div className={classes.imgBox}>
+                  <img src={product.image} className={classes.prodImg} alt="" />
+                </div>
+                <div className={classes.prodInfo}>
+                  <div className={classes.prodName}>{product.title}</div>
+                  <div className={classes.prodPrice}> {product.price}</div>
+                </div>
               </div>
-
-              <div className={classes.prodInfo}>
-                <div className={classes.prodName}>{product.name}</div>
-                <div className={classes.prodPrice}> {product.price}</div>
-              </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
-      {/* <Paging /> */}
     </Fragment>
   );
 };
