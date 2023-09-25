0445945689e3b8e054a228cda553baae0dc71e2d
Lee Gyumin <legm0310@gmail.com>
2023-09-26 08:08:58 +0900

    Feat: getChats

--- client/src/components/chat/Chats.js
+++ client/src/components/chat/Chats.js
@@ -1,35 +1,37 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useCallback, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getRooms } from "../../_actions/chatAction";
 
 import classes from "../../styles/chat/Chat.module.css";
-import test from "../../assets/test.jpg";
+import defaultProfile from "../../assets/defaultProfile.png";
 
 const Chats = () => {
-  Link;
   const dispatch = useDispatch();
   const { rooms } = useSelector((state) => state.chat);
+  const { userId } = useSelector((state) => state.user);
   useEffect(() => {
     dispatch(getRooms());
-  }, []);
+  }, [dispatch]);
   console.log(rooms);
   return (
     <Fragment>
       <div className={classes.chatsWrap}>
         {rooms?.map((room) => (
-          <div key={room.id} className={classes.userChat}>
-            <img src={test} alt="" className={classes.testImg} />
-            <div className={classes.userChatInfo}>
-              <span>{room.RoomUser[0].user_name}</span>
-              <p>{room.chat_logs[0].message}</p>
-              <p>읽지 않은 메시지: {room.unreadCount}</p>
+          <Link to={`/chat/${room.id}`}>
+            <div key={room.id} className={classes.userChat}>
+              <img src={defaultProfile} alt="" className={classes.testImg} />
+              <div className={classes.userChatInfo}>
+                <span>{room.RoomUser[0].user_name}</span>
+                <p>{room.chat_logs[0].content}</p>
+                <p>읽지 않은 메시지: {room.unreadCount}</p>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
 
         <div className={classes.userChat}>
-          <img src={test} alt="" className={classes.testImg} />
+          <img src={defaultProfile} alt="" className={classes.testImg} />
           <div className={classes.userChatInfo}>
             <span>이승훈</span>
             <p>Hello</p>
@@ -37,7 +39,7 @@ const Chats = () => {
         </div>
 
         <div className={classes.userChat}>
-          <img src={test} alt="" className={classes.testImg} />
+          <img src={defaultProfile} alt="" className={classes.testImg} />
           <div className={classes.userChatInfo}>
             <span>김준현</span>
             <p>Hello</p>
@@ -45,7 +47,7 @@ const Chats = () => {
         </div>
 
         <div className={classes.userChat}>
-          <img src={test} alt="" className={classes.testImg} />
+          <img src={defaultProfile} alt="" className={classes.testImg} />
           <div className={classes.userChatInfo}>
             <span>이규민</span>
             <p>Hello</p>
@@ -53,7 +55,7 @@ const Chats = () => {
         </div>
 
         <div className={classes.userChat}>
-          <img src={test} alt="" className={classes.testImg} />
+          <img src={defaultProfile} alt="" className={classes.testImg} />
           <div className={classes.userChatInfo}>
             <span>성우상</span>
             <p>Hello</p>
@@ -61,7 +63,7 @@ const Chats = () => {
         </div>
 
         <div className={classes.userChat}>
-          <img src={test} alt="" className={classes.testImg} />
+          <img src={defaultProfile} alt="" className={classes.testImg} />
           <div className={classes.userChatInfo}>
             <span>전준영</span>
             <p>Hello</p>
@@ -69,7 +71,7 @@ const Chats = () => {
         </div>
 
         <div className={classes.userChat}>
-          <img src={test} alt="" className={classes.testImg} />
+          <img src={defaultProfile} alt="" className={classes.testImg} />
           <div className={classes.userChatInfo}>
             <span>최병준</span>
             <p>Hello</p>
