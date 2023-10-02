96f55eb6d00718f978e11881163dd0ea3e5b1dec
Lee Gyumin <legm0310@gmail.com>
2023-10-02 14:02:49 +0900

    Feat: 채팅 로딩관련 로직 추가

--- client/src/components/chat/Chats.js
+++ client/src/components/chat/Chats.js
@@ -1,82 +1,46 @@
 import React, { Fragment, useCallback, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
 import { getRooms } from "../../_actions/chatAction";
 
 import classes from "../../styles/chat/Chat.module.css";
 import defaultProfile from "../../assets/defaultProfile.png";
 
 const Chats = () => {
+  console.log("chatRender");
   const dispatch = useDispatch();
   const { rooms } = useSelector((state) => state.chat);
-  const { userId } = useSelector((state) => state.user);
-  useEffect(() => {
-    dispatch(getRooms());
-  }, [dispatch]);
-  console.log(rooms);
+  const { userId } = useSelector((state) => state.user, shallowEqual);
+  const { authCheck } = useSelector((state) => state.user, shallowEqual);
+  const { roomId } = useParams() ?? { roomId: 0 };
+
+  // useEffect(() => {
+  //   if (!userId) return;
+  //   if (!rooms || rooms.length === 0) dispatch(getRooms());
+  // }, [dispatch]);
+
   return (
     <Fragment>
       <div className={classes.chatsWrap}>
         {rooms?.map((room) => (
-          <Link to={`/chat/${room.id}`}>
-            <div key={room.id} className={classes.userChat}>
+          <Link
+            key={room.id}
+            to={roomId == room.id ? `/chat` : `/chat/${room.id}`}
+          >
+            <div className={classes.userChat}>
               <img src={defaultProfile} alt="" className={classes.testImg} />
               <div className={classes.userChatInfo}>
                 <span>{room.RoomUser[0].user_name}</span>
-                <p>{room.chat_logs[0].content}</p>
+                <p>
+                  {room.chat_logs[0].content.length > 14
+                    ? `${room.chat_logs[0].content.substr(0, 15)} ...`
+                    : room.chat_logs[0].content}
+                </p>
                 <p>읽지 않은 메시지: {room.unreadCount}</p>
               </div>
             </div>
           </Link>
         ))}
-
-        <div className={classes.userChat}>
-          <img src={defaultProfile} alt="" className={classes.testImg} />
-          <div className={classes.userChatInfo}>
-            <span>이승훈</span>
-            <p>Hello</p>
-          </div>
-        </div>
-
-        <div className={classes.userChat}>
-          <img src={defaultProfile} alt="" className={classes.testImg} />
-          <div className={classes.userChatInfo}>
-            <span>김준현</span>
-            <p>Hello</p>
-          </div>
-        </div>
-
-        <div className={classes.userChat}>
-          <img src={defaultProfile} alt="" className={classes.testImg} />
-          <div className={classes.userChatInfo}>
-            <span>이규민</span>
-            <p>Hello</p>
-          </div>
-        </div>
-
-        <div className={classes.userChat}>
-          <img src={defaultProfile} alt="" className={classes.testImg} />
-          <div className={classes.userChatInfo}>
-            <span>성우상</span>
-            <p>Hello</p>
-          </div>
-        </div>
-
-        <div className={classes.userChat}>
-          <img src={defaultProfile} alt="" className={classes.testImg} />
-          <div className={classes.userChatInfo}>
-            <span>전준영</span>
-            <p>Hello</p>
-          </div>
-        </div>
-
-        <div className={classes.userChat}>
-          <img src={defaultProfile} alt="" className={classes.testImg} />
-          <div className={classes.userChatInfo}>
-            <span>최병준</span>
-            <p>Hello</p>
-          </div>
-        </div>
       </div>
     </Fragment>
   );
