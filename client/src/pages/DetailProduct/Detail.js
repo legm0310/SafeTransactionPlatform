674c8817d29b24b1cbba08a4644cb072e3457e4d
Lee Gyumin <legm0310@gmail.com>
2023-10-02 22:32:35 +0900

    Fix: 디스패치 로딩 추가

--- client/src/pages/DetailProduct/Detail.js
+++ client/src/pages/DetailProduct/Detail.js
@@ -15,6 +15,7 @@ import { IoCart } from "react-icons/io5";
 import Button from "../../components/common/Button";
 import ProductStore from "../../components/detailProduct/ProductStore";
 import ProductInformation from "../../components/detailProduct/ProductInformation";
+import Loading from "../../components/common/Loading";
 
 import classes from "../../styles/product/Detail.module.css";
 import { useSnackbar } from "notistack";
@@ -30,6 +31,7 @@ const Detail = ({ wish, setWish }) => {
     (state) => state.product.productDetail?.product
   );
   const { loadWishList } = useSelector((state) => state.user);
+  const isLoading = useSelector((state) => state.ui.isLoading);
   const userId = useSelector((state) => state.user.userId);
   const sellerId = productDetail?.seller_id;
   const { productId } = useParams();
@@ -112,60 +114,67 @@ const Detail = ({ wish, setWish }) => {
 
   return (
     <Fragment>
-      <div className={classes.productDetailWrap}>
-        <section className={classes.productDetail}>
-          <div className={classes.productImgWrap}>
-            <DetailSlide className={classes.Slide} />
-          </div>
-
-          <div className={classes.producContentWrap}>
-            <div>
-              <div className={classes.category}>{productDetail?.category}</div>
-              <div className={classes.title}>{productDetail?.title}</div>
-              <div className={classes.price}>
-                {" "}
-                {productDetail?.price.toLocaleString()}
-              </div>
-              <div className={classes.time}>{productDetail?.createdAt}</div>
+      {isLoading ? (
+        <div>
+          <Loading />
+        </div>
+      ) : (
+        <div className={classes.productDetailWrap}>
+          <section className={classes.productDetail}>
+            <div className={classes.productImgWrap}>
+              <DetailSlide className={classes.Slide} />
             </div>
 
-            <div className={classes.buttonWrap}>
-              <div className={classes.putMessageButton}>
-                <Button onClick={() => addWishListHandler()}>
-                  <div className={classes.productPutWrap}>
-                    <div className={classes.productPut}>
-                      <FaHeart />
-                      <span className={classes.buttonText}>찜하기</span>
-                    </div>
-                  </div>
-                </Button>
-
-                <Button onClick={onCreateRoomHandler}>
-                  <div className={classes.productMessageWrap}>
-                    <div className={classes.productMessage}>
-                      <TbMessageCircle2Filled />
-                      <span className={classes.buttonText}>판다톡</span>
-                    </div>
-                  </div>
-                </Button>
-              </div>
-
-              <Button onClick={onPurchaseHandler}>
-                <div className={classes.productPurchaseWrap}>
-                  <div className={classes.productPurchase}>
-                    <IoCart />
-                    <span className={classes.buttonText}>구매하기</span>
-                  </div>
+            <div className={classes.producContentWrap}>
+              <div>
+                <div className={classes.category}>
+                  {productDetail?.category}
                 </div>
-              </Button>
-            </div>
-          </div>
-        </section>
+                <div className={classes.title}>{productDetail?.title}</div>
+                <div className={classes.price}>
+                  {" "}
+                  {productDetail?.price.toLocaleString()}
+                </div>
+                <div className={classes.time}>{productDetail?.createdAt}</div>
+              </div>
 
-        <section className={classes.informationWrap}>
-          <div className={classes.prodInformation}>
-            <div className={classes.prodInfoHeader}></div>
-            {/* <div className={classes.prodInfoButton}>
+              <div className={classes.buttonWrap}>
+                <div className={classes.putMessageButton}>
+                  <Button onClick={() => addWishListHandler()}>
+                    <div className={classes.productPutWrap}>
+                      <div className={classes.productPut}>
+                        <FaHeart />
+                        <span className={classes.buttonText}>찜하기</span>
+                      </div>
+                    </div>
+                  </Button>
+
+                  <Button onClick={onCreateRoomHandler}>
+                    <div className={classes.productMessageWrap}>
+                      <div className={classes.productMessage}>
+                        <TbMessageCircle2Filled />
+                        <span className={classes.buttonText}>판다톡</span>
+                      </div>
+                    </div>
+                  </Button>
+                </div>
+
+                <Button onClick={onPurchaseHandler}>
+                  <div className={classes.productPurchaseWrap}>
+                    <div className={classes.productPurchase}>
+                      <IoCart />
+                      <span className={classes.buttonText}>구매하기</span>
+                    </div>
+                  </div>
+                </Button>
+              </div>
+            </div>
+          </section>
+
+          <section className={classes.informationWrap}>
+            <div className={classes.prodInformation}>
+              <div className={classes.prodInfoHeader}></div>
+              {/* <div className={classes.prodInfoButton}>
               <Button onClick={() => onMenuHandler("productInformation")}>
                 <div
                   className={`${classes.infoButton} ${
@@ -186,32 +195,33 @@ const Detail = ({ wish, setWish }) => {
                 </div>
               </Button>
             </div> */}
-            <div className={classes.productInfo}>
-              <div className={classes.productInfoHeader}>상품정보</div>
-              <div className={classes.ProdinfoExplanation}>
-                <ProductInformation />
+              <div className={classes.productInfo}>
+                <div className={classes.productInfoHeader}>상품정보</div>
+                <div className={classes.ProdinfoExplanation}>
+                  <ProductInformation />
+                </div>
               </div>
-            </div>
-            <div className={classes.productStore}>
-              <div className={classes.productStoreHeader}>판매자정보</div>
-              <div className={classes.ProdStoreExplanation}>
-                <ProductStore />
+              <div className={classes.productStore}>
+                <div className={classes.productStoreHeader}>판매자정보</div>
+                <div className={classes.ProdStoreExplanation}>
+                  <ProductStore />
+                </div>
               </div>
-            </div>
 
-            {/* <div className={classes.ProdinfoExplanation}>
+              {/* <div className={classes.ProdinfoExplanation}>
               {activeMenu === "productStore" && <ProductStore />}
               {activeMenu === "productInformation" && <ProductInformation />}
             </div> */}
-          </div>
-        </section>
+            </div>
+          </section>
 
-        <div className={classes.relationproductWrap}>
-          <div className={classes.relationProduct}>
-            <div className={classes.relationProductHeader}>연관 상품</div>
+          <div className={classes.relationproductWrap}>
+            <div className={classes.relationProduct}>
+              <div className={classes.relationProductHeader}>연관 상품</div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </Fragment>
   );
 };
