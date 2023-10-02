e98fdaff2d7c91622f1f55892015fc8575e1e5d3
cbjoon <qudwns017@naver.com>
2023-10-02 22:03:37 +0900

    Feat: 판매자 정보 판매자 이름 추가

--- client/src/components/detailProduct/ProductStore.js
+++ client/src/components/detailProduct/ProductStore.js
@@ -1,7 +1,30 @@
 import React, { Fragment } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import classes from "../../styles/product/Detail.module.css";
+import defaultProfile from "../../assets/defaultProfile.png";
 
 const ProductStore = () => {
-  return <Fragment>1</Fragment>;
+  const productDetail = useSelector(
+    (state) => state.product.productDetail?.product
+  );
+  return (
+    <Fragment>
+      <div className={classes.prodStoreWrap}>
+        <div>
+          <img src={defaultProfile} alt="" className={classes.testImg} />
+        </div>
+        <div className={classes.sellerName}>
+          <Link
+            className={classes.userInfoLink}
+            to={`/user/${productDetail.seller_id}`}
+          >
+            {productDetail.seller_name}
+          </Link>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default ProductStore;
