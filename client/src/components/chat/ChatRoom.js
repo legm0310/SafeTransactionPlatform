6cce7d24cd5f3c2021a8040fe99764c5182991fd
Lee Gyumin <legm0310@gmail.com>
2023-10-02 14:02:49 +0900

    Feat: 채팅 로딩관련 로직 추가

--- client/src/components/chat/ChatRoom.js
+++ client/src/components/chat/ChatRoom.js
@@ -1,8 +1,21 @@
-import React, { Fragment, useState, useEffect, useRef } from "react";
-import { useParams } from "react-router-dom";
+import React, {
+  Fragment,
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+} from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { io } from "socket.io-client";
-import { addChat, getChats } from "../../_actions/chatAction";
+import {
+  addChat,
+  getChats,
+  resetCurrentChats,
+  updateRecentChats,
+} from "../../_actions/chatAction";
+import { setLoadings } from "../../_actions/uiAction";
+import { dateOrTimeFormat } from "../../utils/dataParse";
 
 import classes from "../../styles/chat/Chat.module.css";
 import { useSnackbar } from "notistack";
@@ -14,80 +27,228 @@ const ChatRoom = () => {
   const { enqueueSnackbar } = useSnackbar();
 
   const dispatch = useDispatch();
-  const { roomId } = useParams();
-  const { userData } = useSelector((state) => state.user.authCheck);
-  const { chats } = useSelector((state) => state.chat);
+  const { roomId } = useParams() ?? { roomId: 0 };
+  const { userId, authCheck } = useSelector((state) => state.user);
+  const { chats, roomInfo, socket, hasMoreChatLoad } = useSelector(
+    (state) => state.chat
+  );
+  const isChatLoading = useSelector((state) => state.ui.isChatLoading);
 
-  const [state, setState] = useState({ message: "", name: "" });
   const [chat, setChat] = useState("");
-  const [socket, setSocket] = useState(null);
+  // const socketRef = useRef(null);
 
+  //소켓연결, 해제
+  // useEffect(() => {
+  //   console.log("ChatRoom mounted");
+  //   if (!userId || !roomId) return;
+  //   if (!socketRef.current) {
+  //     const curSocket = io("localhost:5000", {
+  //       cors: { origin: "*" },
+  //     });
+  //     curSocket.on("connect", () => {
+  //       curSocket.emit("onJoinRoom", roomId);
+  //     });
+  //     curSocket.on("onReceiveSend", ({ user, chat }) => {
+  //       dispatch(
+  //         addChat({
+  //           check_read: false,
+  //           content: chat,
+  //           createdAt: new Date(),
+  //           updatedAt: new Date(),
+  //           type: "text",
+  //           id: Date.now(),
+  //           room_id: roomId,
+  //           sender_id: user.id,
+  //           user: { id: user.id, user_name: user.name },
+  //         })
+  //       );
+  //       // dispatch(updateRecentChats(roomId, chat));
+  //     });
+  //     curSocket.on("onReceiveRead", ({ user, chat }) => { });
+  //     curSocket.on("onUpdateRecentChat", ({ roomId, chat }) => {
+  //       dispatch(updateRecentChats(roomId, chat));
+  //     })
+  //     socketRef.current = curSocket;
+  //   }
+  // }, [dispatch, roomId]);
+
+  // useEffect(() => {
+  //   return () => {
+  //     console.log("ChatRoom unmounted");
+  //     socketRef.current?.disconnect();
+  //     socketRef.current = null;
+  //   };
+  // }, [dispatch, roomId]);
+
+  //채팅방 입장 시 로딩
   useEffect(() => {
-    if (!userData) return;
-
-    const curSocket = io("localhost:5000", {
-      cors: { origin: "*" },
-    });
-    setSocket(curSocket);
-
-    curSocket.on("connect", () => {
-      curSocket.emit("onJoinRoom", roomId);
-    });
-    curSocket.on("onRead", () => {});
-    curSocket.on("onReceive", ({ user, chat }) => {
-      dispatch(
-        addChat({
-          id: Date.now(),
-        })
-      );
-    });
-    // console.log(chatWrapRef.current.scrollTop);
-    console.log("before", roomId);
-    if (roomId) {
+    // if (chats.length === 0) return;
+    if (roomId && roomId != roomInfo?.roomId) {
       const body = {
         roomId: roomId,
         lastId: -1,
         limit: 20,
       };
-      dispatch(getChats(body)).then(() => console.log(roomId, chats));
+      dispatch(getChats(body));
     }
-    // console.log(roomId, chats);
 
     return () => {
-      curSocket.disconnect();
+      // if (chats?.length !== 0) {
+      //   dispatch(resetCurrentChats());
+      // }
     };
-  }, [dispatch, roomId]);
+  }, [dispatch, roomId, chats]);
 
-  const chatWrapRef = useRef();
+  // 채팅 전송
+  const onSubmitHandler = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!userId) return;
+      if (chat.trim() === "")
+        return enqueueSnackbar("메세지를 입력해주세요.", {
+          variant: "error",
+        });
+      if (chat.length > 200)
+        return enqueueSnackbar(
+          `최대 200자까지 가능합니다. (현재 ${chat.length}자)`,
+          {
+            variant: "error",
+          }
+        );
 
+      socket?.emit("onSend", {
+        user: {
+          id: userId,
+          name: authCheck.userData?.user_name,
+        },
+        roomId: roomId,
+        chat,
+      });
+      dispatch(
+        addChat({
+          check_read: true,
+          content: chat,
+          createdAt: new Date().toString(),
+          updatedAt: new Date().toString(),
+          type: "text",
+          id: Date.now(),
+          room_id: roomId,
+          sender_id: userId,
+          user: { id: userId, user_name: authCheck?.userData.user_name },
+        })
+      );
+      // socketRef.current?.emit("test", chat);
+      // dispatch(addChat({
+      //   id:
+      // }))
+      setChat("");
+    },
+    [userId, chat, socket, dispatch, setChat]
+  );
+
+  // shift enter입력시 줄바꿈 (미완성)
+  const onLineChange = (e) => {
+    if (e.shiftKey && e.code === "Enter") {
+      e.preventDefault();
+      console.log(e.code);
+      const currentValue = e.target.value;
+      setChat(currentValue + "\n");
+    }
+  };
+
+  const chatRef = useRef(null);
+  const buttonRef = useRef(null);
+
+  //메시지박스 사이즈 조절
+  const resizeTextareaHeight = useCallback(() => {
+    if (!chatRef.current) return;
+
+    chatRef.current.style.height = "auto";
+    chatRef.current.style.height = chatRef.current?.scrollHeight + "px";
+  }, [chatRef]);
+
+  const chatWrapRef = useRef(null);
+
+  //무한 스크롤 이벤트
+  const scrollEvent = useCallback(() => {
+    if (!chatWrapRef.current) return;
+  }, [dispatch, roomId, chats, hasMoreChatLoad, isChatLoading]);
+
+  // 무한 스크롤 이벤트 등록, 해제
+  useEffect(() => {
+    chatWrapRef.current.scrollTop = -500;
+    // console.log(
+    //   chatWrapRef.current.scrollTop,
+    //   chatWrapRef.current.scrollHeight
+    // );
+    chatWrapRef.current?.addEventListener("scroll", scrollEvent);
+    return () =>
+      chatWrapRef.current?.removeEventListener("scroll", scrollEvent);
+  }, [scrollEvent]);
+
+  const renderChat = () => {
+    return chats?.map((chat, index) => {
+      //url의 roomid가 가져온 채팅들의 roomId와 같으면 렌더링
+      if (chat.room_id == roomId)
+        //나와 상대방 채팅 양쪽으로 나눠서 렌더링
+        return (
+          <div key={index} className={classes.positionForOther}>
+            <h3
+              className={
+                chat.sender_id == userId
+                  ? classes.positionForMe
+                  : classes.positionForOther
+              }
+            >
+              {chat.sender_id != userId ? `${chat.user?.user_name} : ` : null}
+              <span>{chat.content}</span>
+              <br></br>
+              <time>
+                {dateOrTimeFormat(chat.createdAt, "YYYY-MM-DD-HH-MM-SS")}
+              </time>
+            </h3>
+          </div>
+        );
+      else return;
+    });
+  };
   return (
     <Fragment>
       <div className={classes.chatRoomWrap}>
         <div className={classes.chatInfo}>
-          <span>abc</span>
+          {/* db의 roomId와 현재 url의 roomId가 같고 db에서 온 파트너값이 있으면 그 파트너의 user_name값을 채팅방 이름 값에 넣음 */}
+          <span>
+            {roomInfo?.roomId == roomId && roomInfo?.partner
+              ? roomInfo.partner.user_name
+              : null}
+          </span>
           <div className={classes.chatIcons}>
             <IoCamera className={classes.icon} />
             <LuMoreHorizontal className={classes.icon} />
             <IoPersonAdd className={classes.icon} />
           </div>
         </div>
-
-        <div ref={chatWrapRef} className={classes.messagesWrap}></div>
-
+        <div ref={chatWrapRef} className={classes.messagesWrap}>
+          {renderChat()}
+        </div>
         {/* <form className={classes.inputWrap} onSubmit={onMessageSubmit}> */}
-        <form className={classes.inputWrap}>
+        <form className={classes.inputWrap} onSubmit={onSubmitHandler}>
           <input
             type="text"
             name="message"
+            value={chat}
+            ref={chatRef}
+            rows={1}
             placeholder="메시지를 입력해주세요"
-            // onChange={(e) => onTextChange(e)}
-            onChange={null}
-            // value={state.message}
-            value={null}
+            // onInput={resizeTextareaHeight}
+            onChange={(e) => setChat(e.target.value)}
+            onKeyDown={onLineChange}
           />
 
           <div className={classes.send}>
-            <button>Send</button>
+            <button type="submit" ref={buttonRef}>
+              Send
+            </button>
             <IoMdAttach className={classes.inputIcon} />
             <input type="file" style={{ display: "none" }} id="file" />
             <label htmlFor="file">
@@ -101,53 +262,3 @@ const ChatRoom = () => {
 };
 
 export default ChatRoom;
-
-// const onTextChange = (e) => {
-//   setState({ ...state, [e.target.name]: e.target.value });
-// };
-
-// let body = {
-//   content: state.message,
-//   check_read: 1,
-//   sender_id: userId,
-//   room_id: roomId,
-// };
-
-// const onMessageSubmit = (e) => {
-//   e.preventDefault();
-
-//   if (state.message.trim() === "") {
-//     enqueueSnackbar("메세지를 입력해주세요.", {
-//       variant: "error",
-//     });
-//     return;
-//   }
-
-//   const { name, message } = state;
-//   console.log({ userId, message });
-
-//   dispatch(addChat(body)).then((response) => {
-//     console.log(response);
-//     if (response.payload.addMessageSuccess) {
-//       console.log({ userId, message });
-//     } else {
-//       enqueueSnackbar("메세지 전송에 실패했습니다.", {
-//         variant: "error",
-//       });
-//       return;
-//     }
-//   });
-
-//   socket?.emit("message", { userId, message });
-//   setState({ message: "", userId });
-// };
-
-// const renderChat = () => {
-//   // return chat.map(({ userId, message }, index) => (
-//   //   <div key={index}>
-//   //     <h3>
-//   //       {userId}:<span>{message}</span>
-//   //     </h3>
-//   //   </div>
-//   // ));
-// };
--- client/src/pages/Chat/Chat.js
+++ client/src/pages/Chat/Chat.js
@@ -1,21 +1,107 @@
-import React, { Fragment, useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import io from "socket.io-client";
-import { TextField } from "@mui/material";
+import React, { Fragment, useCallback, useEffect, useRef } from "react";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import Sidebar from "../../components/chat/Sidebar";
-import ChatRoom from "../../components/chat/ChatRoom";
+import ChatRoom from "../../components/chat/ChatRoom.js";
 
 import classes from "../../styles/chat/Chat.module.css";
+import { useParams } from "react-router-dom";
+import {
+  addChat,
+  getChats,
+  getRooms,
+  socketInit,
+  updateRecentChats,
+} from "../../_actions/chatAction";
+import { io } from "socket.io-client";
 
-const Chat = (props) => {
+const Chat = () => {
+  const dispatch = useDispatch();
+  const { roomId } = useParams() ?? { roomId: 0 };
+  const { userId } = useSelector((state) => state.user);
+  const { rooms, chats, roomInfo, socket } = useSelector((state) => state.chat);
+  const socketRef = useRef(null);
+  // const isChatPageLoading = useSelector((state) => state.ui.isChatPageLoading);
+  // const isChatLoading = useSelector((state) => state.ui.isChatLoading);
+  const fetchRoomData = useCallback(() => {
+    dispatch(getRooms());
+  }, [dispatch, rooms?.length, rooms?.chat_logs?.[0].content, chats?.length]);
+
+  const fetchChatData = useCallback(() => {
+    dispatch(
+      getChats({
+        roomId: roomId,
+        lastId: -1,
+        limit: 20,
+      })
+    );
+  }, [chats?.length, roomId]);
+
+  useEffect(() => {
+    dispatch(getRooms());
+  }, [dispatch]);
+  // }, [dispatch, fetchRoomData]);
+
+  // useEffect(() => {
+  //   console.log(roomId);
+  //   if (roomId && roomId != roomInfo?.roomId) {
+  //     fetchChatData();
+  //   }
+  // }, [dispatch, fetchChatData]);
+
+  useEffect(() => {
+    if (!userId) return;
+    console.log(socketRef.current);
+    if (!socketRef.current) {
+      const curSocket = io("localhost:5000", {
+        cors: { origin: "*" },
+      });
+      curSocket.on("connect", () => {
+        rooms?.forEach((room) => curSocket?.emit("onJoinRoom", room.id));
+      });
+      curSocket.on("onReceiveSend", ({ user, chat, roomId }) => {
+        dispatch(
+          addChat({
+            check_read: false,
+            content: chat,
+            createdAt: new Date().toString(),
+            updatedAt: new Date().toString(),
+            type: "text",
+            id: Date.now(),
+            room_id: roomId,
+            sender_id: roomInfo.partner.id,
+            user: {
+              id: roomInfo.partner.id,
+              user_name: roomInfo.partner.user_name,
+            },
+          })
+        );
+        console.log("업데이트가 될것이다");
+        dispatch(updateRecentChats({ roomId: roomId, chat: chat }));
+      });
+      curSocket.on("onReceiveRead", ({ user, chat }) => {});
+      curSocket.on("onUpdateRecentChat", ({ roomId, chat }) => {
+        console.log("이건 따로 핸들러를 또 만들었음");
+        dispatch(updateRecentChats({ roomId: roomId, chat: chat }));
+      });
+      socketRef.current = curSocket;
+      dispatch(socketInit(curSocket));
+    }
+    // return () => {
+    //   socketRef.current?.disconnect();
+    //   socketRef.current = null;
+    // };
+  }, [dispatch]);
+
   return (
     <Fragment>
+      {/* {false ? null : ( */}
       <div className={classes.ChatWrap}>
         <div className={classes.container}>
           <Sidebar />
-          <ChatRoom />
+          {!roomId ? null : <ChatRoom />}
         </div>
       </div>
+      {/* )} */}
     </Fragment>
   );
 };
