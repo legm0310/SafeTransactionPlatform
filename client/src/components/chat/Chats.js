ea3933178360ca830e44ea84a8d220b61dce6de4
Lee Gyumin <legm0310@gmail.com>
2023-10-03 16:47:46 +0900

    Refactor: 미사용 코드

--- client/src/components/chat/Chats.js
+++ client/src/components/chat/Chats.js
@@ -1,23 +1,13 @@
 import React, { Fragment, useCallback, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useSelector, useDispatch, shallowEqual } from "react-redux";
-import { getRooms } from "../../_actions/chatAction";
 
 import classes from "../../styles/chat/Chat.module.css";
 import defaultProfile from "../../assets/defaultProfile.png";
 
 const Chats = () => {
-  console.log("chatRender");
-  const dispatch = useDispatch();
-  const { rooms } = useSelector((state) => state.chat);
-  const { userId } = useSelector((state) => state.user, shallowEqual);
-  const { authCheck } = useSelector((state) => state.user, shallowEqual);
   const { roomId } = useParams() ?? { roomId: 0 };
-
-  // useEffect(() => {
-  //   if (!userId) return;
-  //   if (!rooms || rooms.length === 0) dispatch(getRooms());
-  // }, [dispatch]);
+  const { rooms } = useSelector((state) => state.chat);
 
   return (
     <Fragment>
