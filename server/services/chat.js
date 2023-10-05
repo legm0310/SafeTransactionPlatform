fa42766a692dbaeb291f59154eb78fe792be3ae5
Lee Gyumin <legm0310@gmail.com>
2023-10-06 06:51:44 +0900

    Test: 테스트 서비스 코드 제거

--- server/services/chat.js
+++ server/services/chat.js
@@ -11,38 +11,11 @@ class ChatService {
     this.ChatLog = Container.get("chatLogModel");
   }
 
-  async createTestRoom(roomData) {
-    const user = await this.User.findByPk(1);
-    //user가 속한 모든 Room을 가져옴.
-    //그중 model과 RoomUser라는 별칭을 사용해서 해당 별칭으로 M:N관계 설정을 했었던 테이블을
-    //가져와서 그 중 id값이 2번인 사용자의 id와 user_name을 가져옴.
-    //그리고 바깥쪽의 attributes는 가져온 Room 중 id가 2번인 유저가 속한 Room의 id만 가져옴
-
-    // 정리 --> user가 속한 모든 Room중에서 id가 2번인 유저의 id, user_name, 그리고 그가 속한 채팅방 Id를 가져옴
-    const isRoomExists = await user.getUserRoom({
-      attributes: ["id"],
-      include: [
-        {
-          model: this.User,
-          as: "RoomUser",
-          where: {
-            id: {
-              [Op.eq]: 2,
-            },
-          },
-          attributes: ["id", "user_name"],
-        },
-      ],
-      raw: true,
-    });
-    console.log(isRoomExists);
-    return;
-  }
-
   async createRoom(roomData) {
     const userId = +roomData.userId;
     const sellerId = +roomData.sellerId;
 
+    let room = null;
     let roomId = null;
     let result = "";
 
@@ -66,8 +39,9 @@ class ChatService {
 
     if (isRoomExists.length >= 1) {
       roomId = isRoomExists[0].id;
-      result = "Update Room";
+      result = "updatedRoom";
       await this.updateJoinState(null, 1, userId, roomId);
+      room = roomId;
     } else {
       const createdRoom = await this.ChatRoom.create({
         name: roomData.roomName,
@@ -81,11 +55,11 @@ class ChatService {
 
       await Promise.allSettled([addSellerJoin, addBuyerJoin]);
 
-      roomId = createdRoom.id;
-      result = "Create New Room";
+      room = createdRoom;
+      result = "createdRoom";
     }
 
-    return { roomId, result };
+    return { room, result };
   }
 
   // async getRooms(userId) {
@@ -260,7 +234,7 @@ class ChatService {
       where: {
         id: lastId === -1 ? { [Op.gt]: lastId } : { [Op.lt]: lastId },
         createdAt: {
-          [Op.gt]: lastJoin.updatedAt,
+          [Op.gte]: lastJoin.updatedAt,
         },
       },
       limit: limit,
@@ -308,7 +282,7 @@ class ChatService {
   }
 
   async updateJoinState(role, state, userId, roomId, txnObj) {
-    await this.ChatParticipant.update(
+    const updatedRoom = await this.ChatParticipant.update(
       {
         ...(role ? { role: role } : {}),
         self_granted: state,
@@ -321,8 +295,16 @@ class ChatService {
         ...(txnObj ? { transaction: txnObj } : {}),
       }
     );
-    return;
+    return updatedRoom;
   }
+
+  // async isExistsRoomCheck(roomData) {
+  //   const isExists = await this.ChatParticipant.findOne(
+  //     {where: {
+
+  //     }}
+  //   )
+  // }
 }
 
 module.exports = ChatService;
