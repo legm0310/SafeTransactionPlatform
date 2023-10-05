ecdf4eb03bee3d66f7ada4044e58c95740e1e291
LeeSeungHoon <ysh73900@gmail.com>
2023-10-05 09:54:54 +0900

    Feat: footer

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,71 @@
+import { Fragment } from "react";
+
+import { FiInstagram } from "react-icons/fi";
+import classes from "../../styles/Footer/Footer.module.css";
+import { useSelector } from "react-redux";
+
+const Footer = () => {
+  const isLoading = useSelector((state) => state.ui.isLoading);
+  return (
+    <Fragment>
+      {isLoading ? null : (
+        <footer className={classes.footerWrap}>
+          <div className={classes.footerContent}>
+            <div className={classes.companyInfo}>
+              <h2>PANDA</h2>
+              <p>
+                Joongbu University 정보보호학과
+                <br />
+                팀명: 개발놈들
+                <br />
+                호스팅서비스 제공자: Amazon Web Services(AWS), Vercel
+              </p>
+            </div>
+            <div className={classes.social}>
+              <h3>Social</h3>
+              <ul className={classes.socialList}>
+                <li className={classes.listContent}>
+                  <FiInstagram className={classes["instagramImg"]} />
+                  Instagram(이규민)
+                </li>
+                <li className={classes.listContent}>
+                  <FiInstagram className={classes["instagramImg"]} />
+                  Instagram(이승훈)
+                </li>
+                <li className={classes.listContent}>
+                  <FiInstagram className={classes["instagramImg"]} />
+                  Instagram(성우상)
+                </li>
+                <li className={classes.listContent}>
+                  <FiInstagram className={classes["instagramImg"]} />
+                  Instagram(전준영)
+                </li>
+                <li className={classes.listContent}>
+                  <FiInstagram className={classes["instagramImg"]} />
+                  Instagram(최병준)
+                </li>
+                <li className={classes.listContent}>
+                  <FiInstagram className={classes["instagramImg"]} />
+                  Instagram(김준현)
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h3>Information</h3>
+            </div>
+            <div>
+              <h3>Contact</h3>
+            </div>
+          </div>
+          <div className={classes.footerBottom}>
+            <p>
+              Copyright ⓒ 2023 <span>Joongbu Univ.</span> All Rights Reserved
+            </p>
+          </div>
+        </footer>
+      )}
+    </Fragment>
+  );
+};
+
+export default Footer;
