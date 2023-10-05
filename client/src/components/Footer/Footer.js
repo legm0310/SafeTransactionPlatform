be5f7c71e84d93ff1b794c81855a29a073284aca
Lee Gyumin <legm0310@gmail.com>
2023-10-06 05:29:15 +0900

    Merge branch 'feat/client' of https://github.com/legm0310/SafeTransactionPlatform into feat/client

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -52,11 +52,6 @@ const Footer = () => {
             </div>
             <div>
               <h3>Information</h3>
-              <ul className={classes.infoBox}>
-                <li>사업 소개</li>
-                <li>대표: 이귀민</li>
-                <li>tel: 010-7763-7514</li>
-              </ul>
             </div>
             <div>
               <h3>Contact</h3>
