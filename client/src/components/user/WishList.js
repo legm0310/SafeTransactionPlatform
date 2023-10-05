73b94a424aeed19e2e63cf022fdc4866383afcbf
LeeSeungHoon <ysh73900@gmail.com>
2023-10-05 09:57:51 +0900

    Fix: 찜목록 버그 수정

--- client/src/components/user/WishList.js
+++ client/src/components/user/WishList.js
@@ -9,32 +9,15 @@ import classes from "../../styles/user/WishList.module.css";
 const WishList = () => {
   const dispatch = useDispatch();
   const userId = useSelector((state) => state.user.userId);
-  const { loadWishList } = useSelector((state) => state.user);
 
   useEffect(() => {
+    console.log("render");
     dispatch(getWishList(userId));
   }, [dispatch]);
 
-  console.log(loadWishList);
-
-  console.log("User id = ", userId);
-
   return (
     <Fragment>
-      {loadWishList?.length === 0 ? (
-        <div className={classes.notWishList}>
-          <h2>찜목록에 담긴 상품이 없습니다.</h2>
-          <p>원하는 상품을 찜목록에 담아보세요!</p>
-        </div>
-      ) : (
-        loadWishList?.map((wishItem) => {
-          return (
-            <div key={`key-${wishItem.id}`}>
-              <WishItem wishItem={wishItem} />
-            </div>
-          );
-        })
-      )}
+      <WishItem />
     </Fragment>
   );
 };
