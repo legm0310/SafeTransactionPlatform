08f78b91175f4999460b5bd82f47176338b9e7bf
cbjoon <qudwns017@naver.com>
2023-10-08 21:44:59 +0900

    Feat: UserInfo 컴포넌트 프로필 내용 추가

--- client/src/pages/User/UserInfo.js
+++ client/src/pages/User/UserInfo.js
@@ -1,18 +1,39 @@
 import React, { Fragment, useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { useLocation } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useLocation, useParams } from "react-router-dom";
 import Button from "../../components/common/Button";
 import SaleList from "../../components/user/SaleList";
 import ReservedProduct from "../../components/user/ReservedProduct";
 import PurchasedProduct from "../../components/user/PurchasedProduct";
 import WishList from "../../components/user/WishList";
+import { getUser } from "../../_actions/userAction";
+import { dateFormat } from "../../utils/dataParse";
 
 import classes from "../../styles/user/UserInfo.module.css";
+
+import defaultProfile from "../../assets/defaultProfile.png";
+import { encodeConstructorParamsForImplementation } from "@thirdweb-dev/sdk";
 
 const UserInfo = () => {
   const [activeMenu, setActiveMenu] = useState("SaleList");
   const location = useLocation();
+  const dispatch = useDispatch();
   const { loadWishList } = useSelector((state) => state.user);
+  const { id } = useParams();
+  const userDetail = useSelector((state) => state.user.userDetail?.userData);
+
+  const userName = userDetail?.user_name;
+  const introduce = userDetail?.introduce;
+
+  useEffect(() => {
+    console.log("id", id);
+    dispatch(getUser(id))
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => err);
+    console.log(userDetail);
+  }, []);
 
   useEffect(() => {
     if (location.state && location.state.activeMenu)
@@ -27,8 +48,26 @@ const UserInfo = () => {
     <Fragment>
       <section className={classes.userInfoSection}>
         <div className={classes.userInfoWrap}>
-          <div></div>
-          <div></div>
+          <div className={classes.userProfileWrap}>
+            <img src={defaultProfile} alt="" className={classes.testImg} />
+            <div>{userName}</div>
+          </div>
+          <div className={classes.userDetailProfile}>
+            <div className={classes.userNameWrap}>{userName}</div>
+            <div className={classes.userStateWrap}>
+              <time>
+                {`가입 날짜 : ${dateFormat(
+                  userDetail?.created_at,
+                  "YYYY년 MM월 DD일"
+                )} `}
+              </time>
+              {/* <div>상품 판매 개수</div> */}
+            </div>
+            <div className={classes.userIntroWrap}>{introduce}</div>
+            <div className={classes.userIntroUpdateWrap}>
+              <button>소개글 수정</button>
+            </div>
+          </div>
         </div>
 
         <div className={classes.userInfoMenuWrap}>
