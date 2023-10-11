6b08a6b77ea831b4e9f45abdd8f2c16dfc1828fd
LeeSeungHoon <ysh73900@gmail.com>
2023-10-12 02:00:39 +0900

    Design: 첫번재 섹션 버튼 추가

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,11 @@
 import { Fragment } from "react";
+import { Link } from "react-router-dom";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import Slide from "../../components/Home/HomeSlide";
-import RecentProductsList from "../../components/product/RecentProducts";
+import RecentProductsList from "../../components/Product/RecentProducts";
 
 import classes from "../../styles/home/Home.module.css";
-import mainImg from "../../assets/platformImage1.png";
-import mainImg1 from "../../assets/platformImage4.png";
+import mainImg from "../../assets/mainImg.png";
 
 const Home = (props) => {
   const [text] = useTypewriter({
@@ -18,24 +18,30 @@ const Home = (props) => {
     <Fragment>
       <div className={classes.container}>
         <section className={classes.mainWrap}>
-          <div className={classes.mainTitle}>
-            <div className={classes.typingTop}>
-              <span>{text}</span>
-              <span>
-                <Cursor style={{ fontWeight: "normal" }} />
-              </span>
+          <div className={classes.mainContent}>
+            <div className={classes.mainTitle}>
+              <div className={classes.typingTop}>
+                <span>{text}</span>
+                <span>
+                  <Cursor style={{ fontWeight: "normal" }} />
+                </span>
+              </div>
+              <div className={classes.typingMiddle}>
+                <span>중고거래</span>
+              </div>
+              <div className={classes.typingBottom}>
+                <span>블록체인을 활용해 보다 안전하게!</span>
+              </div>
+              <div className={classes.IntroButton}>
+                <Link to="/banner">
+                  <button>서비스 더 알아보기</button>
+                </Link>
+              </div>
             </div>
-            <div className={classes.typingMiddle}>
-              <span>중고거래</span>
-            </div>
-            <div className={classes.typingBottom}>
-              <span>블록체인을 활용해 보다 안전하게!</span>
-            </div>
-          </div>
 
-          <div className={classes.mainImgWrap}>
-            <img src={mainImg} alt="" className={classes.mainImg} />
-            <img src={mainImg1} alt="" className={classes.mainImg1} />
+            <div className={classes.mainImgWrap}>
+              <img src={mainImg} alt="" className={classes.mainImg} />
+            </div>
           </div>
         </section>
 
