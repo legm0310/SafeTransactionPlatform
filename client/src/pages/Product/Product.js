0ec62b1b84f13af7294090cba9f8d2d765a524d8
LeeSeungHoon <ysh73900@gmail.com>
2023-10-20 21:26:01 +0900

    Fix: className={classes.prdouctCardWrap} 위치 변경

--- client/src/pages/Product/Product.js
+++ client/src/pages/Product/Product.js
@@ -55,9 +55,9 @@ const Product = (props) => {
           <div className={classes.categoryBar}>
             <CategoryBar onCategoryClick={onCategoryClick} />
           </div>
-          <div className={classes.prdouctCardWrap}>
+          <div>
             {searchParams.get("category") ? (
-              <div>
+              <div className={classes.prdouctCardWrap}>
                 <ProductCard filteredProducts={filteredProducts} />
                 {filteredProducts.length !== 0 ? (
                   <Paging
