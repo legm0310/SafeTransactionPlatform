8c43892d42ffc63ecbe6b670b156a99b02a1290d
junhyeon0218 <junhyeon0218@daum.net>
2023-10-25 18:53:30 +0900

    Docs: 불필요한 div삭제, wish카운트 클래스명 추가

--- client/src/pages/Detail/Detail.js
+++ client/src/pages/Detail/Detail.js
@@ -232,7 +232,9 @@ const Detail = () => {
                         <div className={classes.previousProductPut}>
                           <FaHeart />
                           <span className={classes.buttonText}>찜하기</span>
-                          <span>&nbsp;{wishCount}</span>
+                          <span className={classes.wishCount}>
+                            &nbsp;{wishCount}
+                          </span>
                         </div>
                       )}
                     </div>
@@ -268,7 +270,6 @@ const Detail = () => {
 
           <section className={classes.informationWrap}>
             <div className={classes.prodInformation}>
-              <div className={classes.prodInfoHeader}></div>
               <div className={classes.productInfo}>
                 <div className={classes.productInfoHeader}>상품정보</div>
                 <div className={classes.ProdinfoExplanation}>
