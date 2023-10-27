579594dc77b6e019d9901fa7dffe2d399dee824e
Lee Gyumin <legm0310@gmail.com>
2023-10-27 10:17:34 +0900

    Refactor: 구조체 정의 분리

---
+++ contract/contracts/lib/Structs.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 < 0.9.0;
+
+library Structs{
+
+  struct Product {
+    uint32 id;
+    string title;
+    uint256 price;
+    uint32 sellerId;
+    address seller;
+    bytes32 prodHash;
+  } 
+
+}
